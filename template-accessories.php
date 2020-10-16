87e324df1cbd9510bdb26a23171795775596b2cb
Leo Santillan <leocsdev@gmail.com>
2020-10-16 11:47:24 +0800

    Added code to display category group.

--- template-accessories.php
+++ template-accessories.php
@@ -12,7 +12,7 @@
   <div class="row">
     
     <?php 
-      $args = array('post_type' => 'c_press_accessories');
+      $args = array('post_type' => 'cp_accessories');
         $the_query = new WP_Query( $args );
     ?>
 
@@ -24,8 +24,25 @@
     <div class="col-lg-6">
       <h4><?php the_title(); ?></h4>
       
-      <p>Model: <?php echo the_field('model', $post) ?></p>
+      <p>Model: <?php echo the_field('model_number', $post) ?></p>
       <p>Description: <?php echo the_field('description', $post) ?></p>
+      
+      <!-- SHOW ALL CATEGORIES -->
+      <p>
+      Category: 
+      <?php 
+        $terms = get_field('category_group');
+        
+        if( $terms ): ?>
+          <?php foreach( $terms as $term ): ?>
+            <?php echo esc_html( $term->name ); ?>
+            <!-- <p><?php // echo esc_html( $term->description ); ?></p> -->
+            <!-- <a href="<?php // echo esc_url( get_term_link( $term ) ); ?>">View all '<?php // echo esc_html( $term->name ); ?>' posts</a> -->
+            
+          <?php endforeach; ?>
+        <?php endif; ?>
+        </p>
+
       <img style="width: 50%; " src="<?php echo the_field('image', $post); ?>" alt="image">
     </div>
     <?php endwhile; ?>
