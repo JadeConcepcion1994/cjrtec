972f5e35afb75c7f17b1abbdd2fc66b2cb18cc97
Leo <leocsdev@gmail.com>
2020-10-19 16:03:10 +0800

    moved video style to style.css

--- template-videos.php
+++ template-videos.php
@@ -6,71 +6,73 @@
 
 <?php get_header(); ?>
 
-<div class="container">
-  <h1 class="mb-5"><?php the_title(); ?></h1>
+<section id="videos">
+  <div class="container">
+    <h1 class="mb-5"><?php the_title(); ?></h1>
 
-  <div class="row">
-    
-    <?php 
-      $args = array('post_type' => 'cjrtec_videos');
-        $the_query = new WP_Query( $args );
-    ?>
+    <div class="row">
+      
+      <?php 
+        $args = array('post_type' => 'cjrtec_videos');
+          $the_query = new WP_Query( $args );
+      ?>
 
-    <?php if ( $the_query->have_posts() ) : ?>
-    <?php 
-      while ( $the_query->have_posts() ) : $the_query->the_post();
-        $post = get_the_ID();
-    ?>
-    <div class="col-lg-6 mb-5">
-      <div class="card">
-        <div class="card-body">
-          
-          <h5 class="card-title"><?php the_title(); ?></h5>
-          
-          <p class="card-text">
-            Category: 
-              <?php 
-                $term = get_field('video_category'); 
-                if ($term) {
-                  echo esc_html($term->name);
-                }
-              ?>
-          </p>
-          <div class="embed-container">
-            <?php the_field('video_link'); ?>
+      <?php if ( $the_query->have_posts() ) : ?>
+      <?php 
+        while ( $the_query->have_posts() ) : $the_query->the_post();
+          $post = get_the_ID();
+      ?>
+      <div class="col-lg-6 mb-5">
+        <div class="card">
+          <div class="card-body">
+            
+            <h5 class="card-title"><?php the_title(); ?></h5>
+            
+            <p class="card-text">
+              Category: 
+                <?php 
+                  $term = get_field('video_category'); 
+                  if ($term) {
+                    echo esc_html($term->name);
+                  }
+                ?>
+            </p>
+            <div class="embed-container">
+              <?php the_field('video_link'); ?>
+            </div>
           </div>
         </div>
+
+        <!-- <style>
+          .embed-container { 
+            position: relative; 
+            padding-bottom: 56.25%;
+            overflow: hidden;
+            max-width: 100%;
+            height: auto;
+          } 
+
+          .embed-container iframe,
+          .embed-container object,
+          .embed-container embed { 
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+          }
+        </style> -->
       </div>
+      <?php endwhile; ?>
+      
+      <?php wp_reset_query(); ?> 
 
-      <style>
-        .embed-container { 
-          position: relative; 
-          padding-bottom: 56.25%;
-          overflow: hidden;
-          max-width: 100%;
-          height: auto;
-        } 
-
-        .embed-container iframe,
-        .embed-container object,
-        .embed-container embed { 
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-        }
-      </style>
+      <?php else : ?>
+        <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+      <?php endif; ?>
+      
     </div>
-    <?php endwhile; ?>
-    
-    <?php wp_reset_query(); ?> 
-
-    <?php else : ?>
-      <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-    <?php endif; ?>
-    
   </div>
-</div>
+</section>
 
 <?php get_footer(); ?>
