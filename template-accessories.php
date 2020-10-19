9c415c4ea766d94b5b9310bbe7c7879eb9d5d21f
Leo <leocsdev@gmail.com>
2020-10-19 15:53:41 +0800

    modified style on accessories page.

--- template-accessories.php
+++ template-accessories.php
@@ -6,50 +6,52 @@
 
 <?php get_header(); ?>
 
-<div class="container">
-  <h1 class="mb-5"><?php the_title(); ?></h1>
+<section id="accessories">
+  <div class="container">
+    <h1 class="mb-5"><?php the_title(); ?></h1>
 
-  <div class="row">
-    
-    <?php 
-      $args = array('post_type' => 'cp_accessories');
-        $the_query = new WP_Query( $args );
-    ?>
+    <div class="row">
+      
+      <?php 
+        $args = array('post_type' => 'cp_accessories');
+          $the_query = new WP_Query( $args );
+      ?>
 
-    <?php if ( $the_query->have_posts() ) : ?>
-    <?php 
-      while ( $the_query->have_posts() ) : $the_query->the_post();
-        $post = get_the_ID();
-    ?>
-    <div class="col-lg-6 mb-4">
-      <div class="card">
-        <img class="card-img-top" src="<?php echo the_field('image', $post); ?>" alt="card image">
-        <div class="card-body">
-          <h5 class="card-title">
-            <?php the_title(); ?>
-          </h5>
-          <p class="card-text">
-            Model: <?php echo the_field('model_number', $post) ?> <br>
-            Description: <?php echo the_field('description', $post) ?> <br>
-            Type: 
-              <?php 
-                $term = get_field('category_group'); 
-                if ($term) {
-                  echo esc_html($term->name);
-                }
-              ?>
-          </p>
+      <?php if ( $the_query->have_posts() ) : ?>
+      <?php 
+        while ( $the_query->have_posts() ) : $the_query->the_post();
+          $post = get_the_ID();
+      ?>
+      <div class="col-lg-6 mb-4">
+        <div class="card">
+          <img class="card-img-top" src="<?php echo the_field('image', $post); ?>" alt="card image">
+          <div class="card-body">
+            <h5 class="card-title">
+              <?php the_title(); ?>
+            </h5>
+            <p class="card-text">
+              Model: <?php echo the_field('model_number', $post) ?> <br>
+              Description: <?php echo the_field('description', $post) ?> <br>
+              Type: 
+                <?php 
+                  $term = get_field('category_group'); 
+                  if ($term) {
+                    echo esc_html($term->name);
+                  }
+                ?>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-    <?php endwhile; ?>
-    
-    <?php wp_reset_query(); ?> 
+      <?php endwhile; ?>
+      
+      <?php wp_reset_query(); ?> 
 
-    <?php else : ?>
-      <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-    <?php endif; ?>
+      <?php else : ?>
+        <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+      <?php endif; ?>
+    </div>
   </div>
-</div>
+</section>
 
 <?php get_footer(); ?>
