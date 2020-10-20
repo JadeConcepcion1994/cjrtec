8f1cf665bfd5cd45cd1243625bbb1773e7731797
Leo <leocsdev@gmail.com>
2020-10-20 15:43:28 +0800

    modified item layout to center.

--- template-cp-parts.php
+++ template-cp-parts.php
@@ -11,29 +11,28 @@
     <!-- <h1><?php // the_title(); ?></h1> -->
     <?php the_content(); ?>
 
-    <div class="row">
-
+    <div class="row mb-5">
       <?php 
         $args = array('post_type' => 'cp_parts');
           $the_query = new WP_Query( $args );
       ?>
-
       <?php if ( $the_query->have_posts() ) : ?>
       <?php 
         while ( $the_query->have_posts() ) : $the_query->the_post();
           $post = get_the_ID();
       ?>
 
-
-      <div class="col-md-6 col-lg-3 my-3">
-
+      <div class="col-sm-6 col-lg-3 my-3">
         <div class="card h-100 aos-init aos-animate" data-aos="fade-up">
-          
           <div class="card-body">
-            <img src="<?php echo the_field('image', $post); ?>" alt="image">
-            <h5 class="card-title">
-              <?php the_title(); ?>
-            </h5>
+            <div class="card-heading">
+              <img class="mb-3" src="<?php echo the_field('image', $post); ?>" alt="image">
+              
+              <h5 class="card-title">
+                <?php the_title(); ?>
+              </h5>
+            </div>
+
             <div class="card-text">
               <div class="card-text-item">
                 Item#: <?php echo the_field('item_number', $post) ?>
@@ -49,9 +48,7 @@
                     $feature_3 = get_sub_field('feature_3');
                     $feature_4 = get_sub_field('feature_4');
                     $feature_5 = get_sub_field('feature_5');
-                    
                     ?>
-                    
                     <!-- Display field values. -->
                     <ul>
                       <li><?php echo $feature_1; ?></li>
@@ -73,11 +70,10 @@
                   }
                 ?>
               </div>
-              
-              <a href="#login" class="card-text-cta btn btn-success">View Price</a>
-              
             </div>
           </div>
+
+          <a href="#login" class="card-cta btn btn-success">View Price</a>
         </div>        
       </div>
 
