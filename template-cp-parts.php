7c74fc2325bc25896c9f1864e36fcf6d1170f7fb
Leo <leocsdev@gmail.com>
2020-10-20 11:32:24 +0800

    Fix h1 style and margin.

--- template-cp-parts.php
+++ template-cp-parts.php
@@ -6,7 +6,11 @@
 
 <?php get_header(); ?>
 
-<h1><?php the_title(); ?></h1>
-
+<section id="cp-parts">
+  <div class="container">
+    <!-- <h1><?php // the_title(); ?></h1> -->
+    <?php the_content(); ?>
+  </div>
+</section>
 
 <?php get_footer(); ?>
