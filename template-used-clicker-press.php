86c5f30b79f3ad26ab8c3886c62d6ec9220630f4
Leo <leocsdev@gmail.com>
2020-10-21 16:08:09 +0800

    Added used clicker press page template.

---
+++ template-used-clicker-press.php
@@ -0,0 +1,29 @@
+<?php
+  /*
+  Template Name: Used Clicker Press
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="used-clicker-press">
+  <div class="container">
+    <h1>Used Clicker Presses</h1>
+
+    <div class="row mb-5 d-flex">
+      <div class="col-md-5 d-flex mb-lg-0">
+        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/used-clicker-press.png" alt="Used Clicker Press">
+      </div>
+
+      <div class="col-md-7">
+        <p>Over 80&percnt; of the used machines in the market are discontinued from being manufactured — which means that replacement parts for these machines is hard, or worse, cannot be obtainable anymore. We realize that there may be some good used machines available but if you can't get parts, it's just a matter of time before an expensive investment becomes worthless.</p>
+
+        <p>Quite frankly, this is why there are so many tired and worn out machines on the market. Large manufacturers are dumping their older presses before it's too late and instead are investing in machines with warranties and available parts. They don't want to be the ones holding the bag when an old press finally breaks down.</p>
+
+        <p>If you've been looking at used machines, insist on getting the contact information of the actual manufacturer and call them to verify that they are still in business and is willing to send service information and parts in the event of a break down. Then you should ask the seller to give you a written one-year warranty for your protection.</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
