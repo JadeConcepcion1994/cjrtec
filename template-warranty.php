32677fb1327262fbcf7b23c7e5c39d455e90ae22
Leo <leocsdev@gmail.com>
2020-10-21 14:46:42 +0800

    Added warranty page template.

---
+++ template-warranty.php
@@ -0,0 +1,59 @@
+<?php
+  /*
+  Template Name: Warranty
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="warranty">
+  <div class="container">
+    <div class="row my-5">
+      <div class="col-md-4 d-md-block">
+        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/warranty.jpg" alt="Warranty">
+        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/used clicker press.jpg" alt="Used Clicker Press">
+        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/clicker-press-parts.jpg" alt="Clicker Press Parts">
+        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/cutting_boards.jpg" alt="Cutting Boards">
+      </div>
+      <div class="col-md-8">
+        <h1 class="mb-4">Warranty</h1>
+
+        <p>When a company produces a high-quality product, it makes sense to provide a warranty with a corresponding value. <strong>At CJRTec, we do just that!</strong></p>
+
+        <p>We combine components from Europe, Asia and America to produce the finest precision presses made. We are helping to bring manufacturing back to America and are doing our part to provide the tools American companies need to compete with foreign interests.</p>
+
+        <p>By integrating the vast experience of our own engineers with the support of our world-wide supply partners, we have been able to dominate the market place as the first manufacturer of USA designed clicker press machines with assembly right here in America. Plus, we monitor and manage each part of our production and provide warranty to our clicker presses for a full 12 months. This is what allows us to stand so confidently behind our machines.</p>
+
+        <p>The distance, delay and danger of purchasing from unknown, foreign companies is simply not worth the headache and potential risk – not when there is an American clicker press manufacturing company that can service your needs and be close at hand when you need help.</p>
+
+        <p class="mb-4"><strong>Great products, great warranty, peace of mind – that’s CJRTec.</strong></p>
+
+        <ul>
+          <li>
+            <a href="<?php echo site_url(); ?>/cjrterms/">Read our terms and conditions 
+              <i class="fa fa-chevron-right"></i>
+            </a>
+          </li>
+          <li>
+            <a href="<?php echo site_url(); ?>/about/">Learn more about us 
+              <i class="fa fa-chevron-right"></i>
+            </a>
+          </li>
+          <li>
+            <a href="<?php echo site_url(); ?>/clicker-press-parts/">Shop clicker press parts 
+              <i class="fa fa-chevron-right"></i>
+            </a>
+          </li>
+          <li>
+          <a href="<?php echo site_url(); ?>/clicker-press/">Shop swing arm press and other machines 
+            <i class="fa fa-chevron-right"></i>
+          </a>
+          </li>
+        </ul>
+        
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
