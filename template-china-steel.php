1942643c0fb55977a2ca9195ce65b67e592b4aca
Leo <leocsdev@gmail.com>
2020-10-21 13:18:07 +0800

    Added china steel page template.

---
+++ template-china-steel.php
@@ -0,0 +1,23 @@
+<?php
+  /*
+  Template Name: China Steel
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="china-steel">
+  <div class="container">
+    <h1>Steel Quality And Clicker Press From China</h1>
+
+    <h2 class="mb-3">When it Comes to Clicker Presses, It’s All About the Steel</h2>
+
+    <img class="img-fluid mx-auto d-block mb-5" src="<?php echo get_template_directory_uri(); ?>/assets/img/china-steel.jpg" alt="China Steel">
+
+    <p>China has a well-known reputation for re-producing goods of all kinds. Most of these products are of good quality – however when it comes to products produced from steel, China struggles. Simply put, China has an inability to produce quality steel.</p>
+
+    <p class="mb-5">Clicker presses test the limits of steel because they focus tons of pressure onto the surface area of a few square feet of the cutting board. With this kind of force being applied, it is important to think twice about where the machine is manufactured and the type of steel used. It’s interesting to note that the majority of Chinese factories don’t buy presses made in China for this very reason.</p>
+  </div>
+</section>
+
+<?php get_footer(); ?>
