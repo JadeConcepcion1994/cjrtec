6e8b37a41b1d600241b4f3d3a773a42abd0b830f
Leo <leocsdev@gmail.com>
2020-10-21 09:29:48 +0800

    Added cutting board page template. Modified style for table in cutting board page template.

---
+++ template-cutting-boards.php
@@ -0,0 +1,64 @@
+<?php
+  /*
+  Template Name: Cutting Boards
+  */
+?>
+
+<?php get_header(); ?>
+  
+<section id="cutting-boards">
+  <div class="container">
+    <h1>Clicker Press Cutting Board</h1>
+
+    <p>The cutting board on your clicker press plays a critical role in the speed and quality of the output of your job. The efficiency of the cutting and throughput of the material is directly affected by three main factors. These are: the type of press, the kind of die and the material from which the cutting board is made.</p>
+
+    <p>Many press users go years before discovering that a different type of die or cutting board material can significantly improve their production time and output quality. If a press is going to be cutting many different kinds of material, a nylon board has proven to be the most versatile. However, if a press will be cutting the same material everyday, it makes sense to fine-tune that operation and experiment with different kinds of cutting board material to find what works the best. Other types of cutting boards include; polypropylene, polycarbonate, natural nylon, cast nylon, PVC, and copper.</p>
+
+    <p>At CJRTec, we recommend using nylon boards, when it comes time for replacement. We also recommend that our customers find a local plastics supplier to source their cutting boards. Using a supplier that is close to the shop will reduce the shipping costs of the large and heavy cutting boards.</p>
+
+    <p>The following shows some technical data on a number of different kinds of cutting board materials.</p>
+
+    <div class="table-responsive-md my-5">
+      <table class="table table-striped">
+        <thead>
+          <tr class="table-success">
+          <th class="text-center">Type of Material</th>
+          <th class="text-center">Product Name</th>
+          <th class="text-center">Density</th>
+          <th class="text-center">Tensile Strength</th>
+          <th class="text-center">Heat Deflection &commat; 66 psi</th>
+          <th class="text-center">Shore D Hardness &lpar;est&rpar;</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Nylon</td>
+            <td>NYCAST 6PA Natural</td>
+            <td class="text-right">1.15-1.17 g&sol;cm&sol;3</td>
+            <td class="text-right">10,000-13,500 psi</td>
+            <td class="text-right">300-400&deg;F</td>
+            <td class="text-right">84</td>
+          </tr>
+          <tr>
+            <td>White polypropylene</td>
+            <td>PROTEC PP</td>
+            <td class="text-right">0.9 g&sol;cm3</td>
+            <td class="text-right">4,900 psi</td>
+            <td class="text-right">210&deg;F</td>
+            <td class="text-right">78</td>
+          </tr>
+          <tr>
+            <td>Grey polypropylene</td>
+            <td>FORMOLENE 5100J</td>
+            <td class="text-right">0.9 g&sol;cm3</td>
+            <td class="text-right">4,900 psi</td>
+            <td class="text-right">210&deg;F</td>
+            <td class="text-right">72</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
