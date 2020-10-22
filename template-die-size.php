0984e2d76b2d356bda99d0137b42ea2d8ea52680
Leo <leocsdev@gmail.com>
2020-10-22 15:48:56 +0800

    Added die size page template.

---
+++ template-die-size.php
@@ -0,0 +1,35 @@
+<?php
+  /*
+  Template Name: Die Size  
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="die-size" class="mb-5">
+  <div class="container">
+    <h1 class="text-center">Die Size Page Template</h1>
+
+    <p>The size of your die will often determine the type of press you will need and the tonnage. It also determines the ultimate cost per piece of the product you are cutting. In the die cutting world, everything is driven by cost per piece. If you have a press with a 19 x 24&quot; cutting head and you are cutting out 9&quot; circles, you can cut 4 circles with every stroke providing the press is strong enough to cut 4 circles depending on the material you are cutting.</p>
+
+    <p>However, it takes nearly the same labor to cut 4 circles with a swing arm press as it does to cut 21 circles with a 30 x 64&quot; beam press. Once you have paid for a press, your highest cost will be labor. The more items you can cut per stroke, the less your cost per piece will be. As a rule, you're always better to buy the biggest press you can afford (which is a onetime purchase) than to bear the expense of labor forever.</p>
+
+    <p>It&apos;s important to remember that the larger your die, the more pressure you will need to complete the cut. This is what causes a lot of uncertainty with prospective buyers. They have no idea how many tons they will need to cut a given material with a specific die. Everyone hopes for a formula so they can measure the linear inches of steel rule and enter in a factor for the material they are cutting. It’s not quite this simple but we can give you a bit of help.</p>
+
+    <a href="#tonnage-formula">
+      Click here to see our tonnage formula for help with this 
+      <i class="fa fa-chevron-right"></i>
+    </a>
+
+    <p class="mt-5">Everything eventually gets down to the cost per piece you can live with and it&apos;s particular production requirement. If you can run your press slowly, a smaller machine may work – but if you need high output and the cheapest price possible, you&apos;ll need a larger press and perhaps a full automatic press.</p>
+
+    <p>That being said, we&apos;re back to the size of your die. First, consider the dimensions of the item you are cutting and then determine how many items you can get on a die based on the size press that fits your budget. Swing arm presses run between 10 and 27 tons with a cutting head from 11&quot; x 14.5 to 24&quot; x 24&quot;.</p>
+
+    <a href="#die-consideration">
+      Click here to see more die considerations 
+      <i class="fa fa-chevron-right"></i>
+    </a>
+  </div>
+</section>
+
+<?php get_footer(); ?>
