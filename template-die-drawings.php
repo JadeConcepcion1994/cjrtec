94e3456c3e94c0d00defd5ef564674ebe065372e
Leo <leocsdev@gmail.com>
2020-10-22 15:21:15 +0800

    Added die drawings page template and assets.

---
+++ template-die-drawings.php
@@ -0,0 +1,132 @@
+<?php
+  /*
+  Template Name: Die Drawings
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="die-drawings">
+  <div class="drawing-design mt-5">
+    <div class="container">
+      <div class="row d-flex">
+        <div class="col-md-6 col-img">
+          <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/steel-die-CAD-drawing.jpg" alt="CAD Drawing of Steel Die - made with AutoCAD&circledR;">
+        </div>
+
+        <div class="col-md-6 col-text">
+          <h1>Die Drawings and Design</h1>
+
+          <p>The first thing a die maker will ask for is a drawing. Most drawings are done in AutoCAD&circledR; or SolidWorks&circledR;, but programs like Adobe&circledR; Photoshop&circledR;, Illustrator&circledR; or CorelDRAW&circledR; can also be used. Regardless of the program you use, you will need to provide a drawing that calls out every aspect of your drawing and you will need to show the location of your registration pins.</p>
+
+          <a href="">
+            Learn more about registration pins <i class="fa fa-chevron-right"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="clicker-die pt-5 mb-5">
+    <div class="container">
+      <h2>What is a clicker die?</h2>
+
+      <p>A <strong>clicker die</strong>, or simply a &quot;die&quot;, is an important <a href="<?php echo site_url(); ?>/accessories" target="_blank">clicker press accessory</a> used to cut raw materials used in production. The most common material is leather but a clicker press can cut all sorts of soft materials like plastic, rubber, lace, PVC, and fiberglass among others.</p>
+
+      <p><a href="#clicker-press-parts" target="_blank">Clicker dies come in different sizes, forms, and shapes</a>. But ultimately, dies can be customized according to your specifications. The most common clicker die is for cutting leather wallets, shoe strings, business cards, puzzles, packaging, and more.</p>
+
+      <ul class="mt-4">
+        <li>
+          <a href="">
+            Looking to have a die custom made for your clicker press? 
+            <i class="fa fa-chevron-right"></i>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            View American clicker press for sale
+            <i class="fa fa-chevron-right"></i>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            Know about our FULL one-year warranty for all Clicker Presses
+            <i class="fa fa-chevron-right"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="clicker-die-samples mb-5">
+    <div class="container">
+      <h2>Clicker Die Samples</h2>
+
+      <div class="row mt-4">
+        <div class="col-md-6 col-lg-4 mb-3">
+          <a href="#beam-press">
+            <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/die-big-3-2.jpg" alt="Beam Clicker Press Die">
+            <h4 class="mt-2 text-center">Beam Clicker Press Die</h4>
+          </a>
+        </div>
+
+        <div class="col-md-6 col-lg-4 mb-3">
+          <a href="#receding-beam-press">
+            <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/die-big-4-2.jpg" alt="Receding Head Press Die">
+            <h4 class="mt-2 text-center">Receding Head Press Die</h4>
+          </a>
+        </div>
+
+        <div class="col-md-6 col-lg-4 mb-3">
+          <a href="#conveyor-press">
+            <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/die-big-13-1.jpg" alt="Clicker Die for Conveyor Beam Press">
+            <h4 class="mt-2 text-center">Clicker Die for Conveyor Beam Press</h4>
+          </a>
+        </div>
+
+        <div class="col-md-6 col-lg-4 mb-3">
+          <a href="#swing-arm-press">
+            <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/die-big-16-2.jpg" alt="Leather Clicker Die for Swing Arm Press">
+            <h4 class="mt-2 text-center">Leather Clicker Die for Swing Arm Press</h4>
+          </a>
+        </div>
+
+        <div class="col-md-6 col-lg-4 mb-3">
+          <a href="#swing-arm-press">
+            <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/die-big-16-1.jpg" alt="Leather Goods Cut with Swing Arm Clicker Die">
+            <h4 class="mt-2 text-center">Leather Goods Cut with Swing Arm Clicker Die</h4>
+          </a>
+        </div>
+
+        <div class="col-md-6 col-lg-4 mb-3">
+          <a href="#traveling-head-press">
+            <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/diecut-puzzle-pieces.jpg" alt="Puzzle Pieces Die for Travel Head Press">
+            <h4 class="mt-2 text-center">Puzzle Pieces Die for Travel Head Press</h4>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="cta-strip py-5">
+    <div class="container">
+      <h2>Create an Account with CJRTec Now</h2>
+      <a href="#registration" class="btn-cta-strip">
+        Get Started Today <i class="fa fa-angle-right"></i>
+      </a>
+    </div>
+  </div>
+
+
+
+
+
+  
+
+
+
+
+
+</section>
+
+<?php get_footer(); ?>
