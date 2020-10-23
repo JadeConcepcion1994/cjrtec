a086e141231262ac90e8f38e7868110dd9f913f6
Leo <leocsdev@gmail.com>
2020-10-23 08:57:24 +0800

    Added registration pins page template and assets.

---
+++ template.-registration-pins.php
@@ -0,0 +1,31 @@
+<?php
+  /*
+  Template Name: Registration Pins
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="registration-pins">
+  <div class="container">
+    <h1 class="text-center">Registration Pins</h1>
+
+    <div class="row mb-5">
+      <div class="col-md-5">
+        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/registration pins.jpg" alt="Registration pins">
+
+        <p class="mt-4 mb-5">Registration pins are used for aligning printed material so you're cutting in the right place every time. The pins are spring backed so they can be pushed down without damaging the press or die.</p>
+      </div>
+
+      <div class="col-md-7">
+        <div class="embed-container">
+          <iframe width="900" height="506" src="https://www.youtube.com/embed/_5sprjhXBfo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+
+        <h4 class="text-center mt-3">Die Alignment Pins</h4>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
