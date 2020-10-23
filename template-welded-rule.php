e668e9fd51960337d8ab02ef586be09f39692121
Leo <leocsdev@gmail.com>
2020-10-23 11:29:53 +0800

    Added welded rule page template.

---
+++ template-welded-rule.php
@@ -0,0 +1,23 @@
+<?php
+  /*
+  Template Name: Welded Rule
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="welded-rule">
+  <div class="container">
+    <div class="section-content mb-5">
+      <h1 class="text-center">Welded Rule</h1>
+
+      <p>Although it increases the cost of the die, welding the connecting points of your steel rule will give you a better cut and oftentimes make your die last longer. Every time you make a cut, you are putting pressure against the sides of the die. Depending on how much pressure is exerted, the ejected material will eventually press against the sides enough that a gap will begin to develop. If you get a gap between two ends of the rule, your material will no longer cut in that area.</p>
+
+      <p>If you are cutting with an automatic press or CNC press, you should always request welded rule because it is stronger and less susceptible to pulling out of the wood or acrylic backing. Over time, the steel rule will work its way loose to the point that it can pull out of its backing. If this happens in automatic mode, the rule can cut a die in half or damage your material.</p>
+
+      <p>After the die maker welds the edges, they will grind the blade again to give it a sharp edge. When edges are welded, you have a stronger and safer die.</p>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
