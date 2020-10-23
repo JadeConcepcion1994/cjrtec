e21323eccbaba01e0ae1d34b7d89b75076ef386e
Leo <leocsdev@gmail.com>
2020-10-23 11:12:53 +0800

    Added steel rule dies page template and its assets.

---
+++ template-steel-rule-dies.php
@@ -0,0 +1,37 @@
+<?php
+  /*
+  Template Name: Steel Rule Dies
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="steel-rule-dies">
+  <div class="container">
+    <div class="section-content mb-5">
+      <h1 class="text-center">Steel Rule Dies</h1>
+
+      <p>Steel rule comes in a variety of heights, thicknesses and bevels. Choosing the right steel rule is essential to good die cutting. The thinner the rule, the easier it will cut through your material – however the die will be weaker without backing. The deeper the bevel, the easier the cutting will be also, however the blade will dull faster.</p>
+
+      <p>There is a give and take with all these decisions. If your material is thin, then .937 rule height with backing should work fine, but if your cutting 3&quot; foam, it's simply not going to work. The cavity of the die is not deep enough for the compressed foam.</p>
+
+      <p>There are many types of steel rule. Different thicknesses, different heights and different hardness. Sometimes you choose steel rule because you need a die that is very strong and other times it will be because you need a very tall die cavity. Every cutting objective is different, so there are different types of dies to accommodate the parameters of what your preparing to cut.</p>
+    </div>
+    
+    <div class="row mb-5 d-flex align-items-center">
+      <div class="col-md-6 mb-4">
+        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/die-making.jpg" alt="steel rule">
+      </div>
+      
+      <div class="col-md-6 mb-4">
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Zh3o7IIYDro" allowfullscreen></iframe>
+        </div>
+
+        <h4 class="text-center mt-3">Setting Up Your Die</h4>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
