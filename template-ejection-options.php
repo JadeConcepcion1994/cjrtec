7e231f67b6875bb2f77fd51881f1455717a323dc
Leo <leocsdev@gmail.com>
2020-10-23 11:47:49 +0800

    Added ejection options page template.

---
+++ template-ejection-options.php
@@ -0,0 +1,23 @@
+<?php
+  /*
+  Template Name: Ejection Options
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="ejection-options">
+  <div class="container">
+    <div class="section-content mb-5">
+      <h1 class="text-center">Ejection Options</h1>
+
+      <p>Ejection methods are as important as the other aspects of die making and maintenance. The idea is to design the die in a way that allows for quick cutting, stripping and re-setup for the next cut. If the material is getting stuck in the cavity of the die every time, it is costing you money.</p>
+
+      <p>The challenge with this is that most of the time, workers are having a hard time utilizing clicker dies for weeks, months and years without saying anything. This is clearly a waste of your money. You should keep a close eye on your die cutting so you can detect tasks that are taking longer than they should.</p>
+
+      Some of the most common ejection choices are <strong>foam</strong>, <strong>cork</strong> and <strong>springs</strong>.
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
