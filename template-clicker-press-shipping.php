72c2792567879d9c9c2b48f384eec506d61733c4
Leo <leocsdev@gmail.com>
2020-10-26 08:23:53 +0800

    Added assets for clicker press shipping page.

---
+++ template-clicker-press-shipping.php
@@ -0,0 +1,169 @@
+<?php
+  /*
+  Template Name: Clicker Press Shipping
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="clicker-press-shipping">
+  <div class="container">
+    <h1 class="text-center">Shipping Information and Freight Companies</h1>
+
+    <div class="shipping-content-1 mb-5">
+      <p>We are pleased to assist our customers with competitive shipping from our facility. However, the shipping process is the responsibility of the BUYER. Shipping may be scheduled once payment has been received and funds have been cleared. BILL OF LADING must be received from BUYER 24 hours before the trucking is to arrive.</p>
+
+      <p>Below you will find all the information you will need to hire a shipping company.</p>
+
+      <a href="#clicker-press">
+        Find the weight and dimensions here 
+        <i class="fa fa-chevron-right"></i>
+      </a>
+    </div>
+
+    <div class="shipping-content-2 mb-5">
+      <div class="row">
+        <div class="col-md-3">
+          <h5>PREPARATION</h5>
+        </div>
+        <div class="col-md-9">
+          Unless specifically requested, most presses ship without a crate. Buyer may request a crate for an additional fee. Smaller presses are bolted to a pallet and shrink wrapped.
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col-md-3">
+          <h5>CLASSIFICATION</h5>
+        </div>
+        <div class="col-md-9">
+          Shipping Class 100
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col-md-3">
+          <h5>PICKUP HOURS</h5>
+        </div>
+        <div class="col-md-9">
+          Monday to Friday, between 10:00 AM and 3:00 PM (excluding Holidays)
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col-md-3">
+          <h5>PICKUP ADDRESS</h5>
+        </div>
+        <div class="col-md-9">
+          7023 S 700 W Building B, Midvale UT 84047 USA
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col-md-3">
+          <h5>PHONE</h5>
+        </div>
+        <div class="col-md-9">
+          800-733-2302
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col-md-3">
+          <h5>FORKLIFT</h5>
+        </div>
+        <div class="col-md-9">
+          Shipper has a forklift and will load the goods.
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col-md-3">
+          <h5>LOADING DOCK</h5>
+        </div>
+        <div class="col-md-9">
+          Shipper does not have a loading dock.
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col-md-3">
+          <h5>SIZE RESTRICTIONS</h5>
+        </div>
+        <div class="col-md-9">
+          Presses that are wider than 89" can not be loaded on a standard freight trailer.
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col-md-3">
+          <h5>DRIVER INSTRUCTIONS</h5>
+        </div>
+        <div class="col-md-9">
+          Driver must come into the building through the front door or call 800 733 2302 when ready at the rear of the building.
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col-md-3">
+          <h5>PHOTOS</h5>
+        </div>
+        <div class="col-md-9">
+          All shipments are photographed before being released for the protection of our buyers.
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="shipping-content-3 mb-5">
+      <h2 class="text-center mb-4">Helpful Shipping Information</h2>
+
+      <table class="table mb-4">
+        <thead>
+          <tr>
+            <td>Standard Freight Trailer</td>
+            <td>Dimensions</td>
+            <td>Max Weight</td>
+            <td>Door Dimensions</td>
+          </tr>
+        </thead>
+        <tbody>
+          
+          <tr>
+            <td></td>
+            <td>OD 48&apos; 102&quot;</td>
+            <td>13,500 lbs</td>
+            <td>ID 97&quot; wide by 107&quot; tall</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <img class="img-fluid d-block mx-auto mb-5" src="<?php echo get_template_directory_uri(); ?>/assets/img/trailer-standard.jpg" alt="shipping trailer">
+
+      <table class="table mb-4">
+        <tbody>
+          <tr>
+            <td>Platform Trailer</td>
+            <td>Dimensions</td>
+            <td>Max Weight</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>48&apos; 102&quot;</td>
+            <td>50,000 &dash; 55,000 lbs.</td>
+          </tr>
+        </tbody>
+      </table>
+      
+      <img class="img-fluid d-block mx-auto" src="<?php echo get_template_directory_uri(); ?>/assets/img/trailer-flatbed.jpg" alt="shipping flatbed">
+    </div>
+
+
+
+
+
+
+  </div>
+</section>
+
+<?php get_footer(); ?>
