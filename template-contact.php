46804ba0239748f05d154f6f6feda764cd5dbd49
Leo <leocsdev@gmail.com>
2020-10-28 11:22:26 +0800

    Added contact page template.

---
+++ template-contact.php
@@ -0,0 +1,236 @@
+<?php
+  /*
+  Template Name: Contact
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="contact">
+  <div class="section-heading mb-5">
+    <div class="container">
+      <h1 class="text-center">
+        CJRTEC Contact Number and Mailing Address
+      </h1>
+
+      <p class="text-center">Our operators are standing by ready to answer your calls. Use the following links and numbers to contact us.</p>
+
+      <p class="text-center mb-5">
+        <strong>7023 S 700 W, Building B, Midvale, Utah 84047 USA</strong>
+      </p>
+    </div>
+  </div>
+
+  <div class="section-email pt-4 mb-5">
+    <div class="container">
+      <h2 class="text-center">Email Us</h2>
+
+      <p class="text-center mb-1">
+        You can email us at the following addresses:
+      </p>
+
+      <div class="row">
+        <div class="col-md-6 col-lg-4 my-3">
+          <div class="card h-100">
+            <a href="mailto: info@cjrtec.com">
+              <div class="card-body">
+                <h3 class="card-title text-center">
+                  Customer Services
+                </h3>
+                <p class="card-text text-center">
+                  info@cjrtec.com
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 my-3">
+          <div class="card h-100">
+            <a href="mailto: acc@cjrtec.com">
+              <div class="card-body">
+                <h3 class="card-title text-center">
+                  Accounting Services
+                </h3>
+                <p class="card-text text-center">
+                  acc@cjrtec.com
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 my-3">
+          <div class="card h-100">
+            <a href="mailto: parts@cjrtec.com">
+              <div class="card-body">
+                <h3 class="card-title text-center">
+                  Parts Services
+                </h3>
+                <p class="card-text text-center">
+                  parts@cjrtec.com
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 my-3">
+          <div class="card h-100">
+            <a href="mailto: sales@cjrtec.com">
+              <div class="card-body">
+                <h3 class="card-title text-center">
+                  Sales Services
+                </h3>
+                <p class="card-text text-center">
+                  sales@cjrtec.com
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 my-3">
+          <div class="card h-100">
+            <a href="mailto: support@cjrtec.com">
+              <div class="card-body">
+                <h3 class="card-title text-center">
+                  Support Services
+                </h3>
+                <p class="card-text text-center">
+                  support@cjrtec.com
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 my-3">
+          <div class="card h-100">
+            <a href="mailto: dealer@cjrtec.com">
+              <div class="card-body">
+                <h3 class="card-title text-center">
+                  Dealers Services
+                </h3>
+                <p class="card-text text-center">
+                  dealer@cjrtec.com
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section-call pt-4 mb-5">
+    <div class="container">
+      <h2 class="text-center">Call Us</h2>
+
+      <p class="text-center mb-1">
+        You can call us at the following numbers:
+      </p>
+
+      <div class="row">
+        <div class="col-md-6 col-lg-4 my-3">
+          <div class="card h-100">
+            <a href="tel:(800) 733 2302">
+              <div class="card-body">
+                <h3 class="card-title text-center">
+                  Customer Services
+                </h3>
+                <p class="card-text text-center">
+                  &lpar;800&rpar; 733 2302 <br>
+                  extension dial 101 
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 my-3">
+          <div class="card h-100">
+            <a href="tel: &lpar;800) 733 2302">
+              <div class="card-body">
+                <h3 class="card-title text-center">
+                  Accounting Services
+                </h3>
+                <p class="card-text text-center">
+                  &lpar;800&rpar; 733 2302 <br>
+                  extension dial 103
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 my-3">
+          <div class="card h-100">
+            <a href="tel: (800) 733 2302">
+              <div class="card-body">
+                <h3 class="card-title text-center">
+                  Parts Services
+                </h3>
+                <p class="card-text text-center">
+                  &lpar;800&rpar; 733 2302 <br>
+                  extension dial 105
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 my-3">
+          <div class="card h-100">
+            <a href="tel: (800) 733 2302">
+              <div class="card-body">
+                <h3 class="card-title text-center">
+                  Sales Services
+                </h3>
+                <p class="card-text text-center">
+                  &lpar;800&rpar; 733 2302 <br>
+                  extension dial 103
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 my-3">
+          <div class="card h-100">
+            <a href="tel: (800) 733 2302">
+              <div class="card-body">
+                <h3 class="card-title text-center">
+                  Support Services
+                </h3>
+                <p class="card-text text-center">
+                  &lpar;800&rpar; 733 2302 <br>
+                  extension dial 104
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 my-3">
+          <div class="card h-100">
+            <a href="tel: (800) 733 2302">
+              <div class="card-body">
+                <h3 class="card-title text-center">
+                  Dealers Services
+                </h3>
+                <p class="card-text text-center">
+                  &lpar;800&rpar; 733 2302 <br>
+                  extension dial 106
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</section>
+
+<?php get_footer(); ?>
