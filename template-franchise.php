eba13e1beecd81de7e218eb63cac7bd710281fde
Leo <leocsdev@gmail.com>
2020-10-28 13:54:46 +0800

    Added franchise page template.

---
+++ template-franchise.php
@@ -0,0 +1,25 @@
+<?php
+  /*
+  Template Name: Franchise
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="franchise">
+  <div class="container mb-5">
+    <h1 class="text-center">
+      We Are Open To World Wide Franchising
+    </h1>
+
+    <img class="img-fluid img-thumbnail d-block mx-auto mb-5" src="<?php echo get_template_directory_uri(); ?>/assets/img/franchise-opportunities.png" alt="grab the opportunity and get a franchise from cjrtec">
+
+    <p>If you are an international buyer, we are happy to assist you in any way we can, but additionally, we encourage you to consider for the moment the potential opportunity of representing our line abroad. We, of course, would be looking for someone with the right experience and industry insight, but in a market that is predominantly comprised of cheap Chinese machines, we offer a unique advantage with unmatched quality and distribution capability.</p>
+
+    <a href="#clicker-press" >
+      Find press machines here <i class="fa fa-chevron-right"></i>
+    </a>
+  </div>
+</section>
+
+<?php get_footer(); ?>
