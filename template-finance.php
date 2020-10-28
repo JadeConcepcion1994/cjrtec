02d5752227d78053a282a261f261c4db63850fa4
Leo <leocsdev@gmail.com>
2020-10-28 14:22:09 +0800

    Added finance page template.

---
+++ template-finance.php
@@ -0,0 +1,40 @@
+<?php
+  /*
+  Template Name: Finance
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="finance" class="my-5">
+  <div class="container">
+    <h1 class="text-center">
+      Apply For Financing and Get Approved Fast&ast;
+    </h1>
+
+    <div class="row d-flex justify-content-center align-items-center">
+      <div class="col-md-6 my-4">
+        <img class="img-fluid d-block mx-auto" src="<?php echo get_template_directory_uri(); ?>/assets/img/apply-for-financing-and-get-approved-fast.jpg" alt="apply for heavy equipment financing">
+      </div>
+
+      <div class="col-md-6 my-4">
+        <p>We offer financing on approved credit that can free up capital and get you on your way. We can generally get a decision within 48 hours following receipt of completed application.</p>
+
+        <p><em>&ast; All applications will require a personal guarantee.</em></p>
+
+        <p>Buyers may also look to their local credit union or bank as their first choice of financing as this will almost always result in a more competitive interest rate.</p>
+
+        <p>Occasionally, we are requested by lenders to release equipment to the buyer and allow them to set it up before releasing payment.</p>
+
+        <p>Before going through the application process, please read through our terms of purchase to make sure there is no conflict between your lender's funding and our company&apos;s purchase requirements.</p>
+
+        <a href="#cjrterms">
+          Read our terms and policy 
+          <i class="fa fa-chevron-right"></i>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
