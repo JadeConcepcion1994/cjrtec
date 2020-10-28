1ef084fdfba9c4958ace1a4cfff44abaee2e1b9e
Leo <leocsdev@gmail.com>
2020-10-28 13:01:21 +0800

    Added training page template.

---
+++ template-training.php
@@ -0,0 +1,33 @@
+<?
+  /*
+  Template Name: Training
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="training">
+  <div class="container">
+    <h1 class="section-heading text-center">
+      Die Cutting Machine Training For Beginners
+    </h1>
+    
+    <div class="section-image float-md-left p-4">
+      <img class="img-fluid d-block mx-auto" src="<?php echo get_template_directory_uri(); ?>/assets/img/training.jpg" alt="Training">
+    </div>
+
+    <div class="section-info">
+      <p>If you&apos;re new to clicker press machines, training may be an important factor you have to consider getting after you purchase a press. The machines may seem pretty straightforward, but you wouldn&apos;t believe how much there is to know before actually thinking of operating one. In many ways, what you learn may significantly save your company. Many buy a machine and go years before learning about the cost saving consideration in cutting boards, pre-delivery systems, waste recovery systems, and other tools and accessories that can reduce labor day in and day out.</p>
+
+      <p>CJRTec values knowledge and is anxious to share with anyone who are willing to learn all about clicker presses. We offer training at our Utah center, or offsite if needed. CJRTec also offers a wealth of information online through video training. No matter what your needs are, we have a convenient way to help you.</p>
+
+      <p>For more information contact us <a href="<?php echo site_url(); ?>/contact" target="_blank">here</a>.</p>
+    </div>
+
+    <div class="clearfix mb-5"></div>
+  </div>
+
+  
+</section>
+
+<?php get_footer(); ?>
