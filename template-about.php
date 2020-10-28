1f88bac2ea788020d501cdee30618bfe2d9e943e
Leo <leocsdev@gmail.com>
2020-10-28 09:25:30 +0800

    Added about page template.

---
+++ template-about.php
@@ -0,0 +1,107 @@
+<?php
+  /*
+  Template Name: About
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="about">
+  <div class="section-heading mb-5">
+    <div class="container">
+      <h1 class="text-center">About CJRTEC</h1>
+
+      <h2 class="text-center text-muted mb-5">
+        THE NO. 1 AMERICAN CLICKER PRESS DISTRIBUTOR IN THE WORLD
+      </h2>
+    
+      <div class="row d-flex align-items-center">
+        <div class="col-md-5">
+          <blockquote class="lead text-center">
+            CJRTec is the Leader in Clicker Press Machine Manufacturing and Distribution
+          </blockquote>
+        </div>
+
+        <div class="col-md-7">
+          <p>CJRTec has been in the manufacturing business for more than a decade. We are an international company based in the USA, with sales all over the world and offices in the USA, the Philippines, Korea and China. Our focus is in all types of specialized cutting machines with an emphasis in automated cutting.</p>
+
+          <p><a href="#clicker-press">Most of the machines we sell are custom-built to customer-specific needs</a>. While we are not the sole company in this market segment, we are the only company that can make custom presses from a 10 ton to a 350 ton – in a variety of table sizes and machine types.</p>
+
+          <p>CJRTec enjoys annual sales that are nearly twice that of all USA-based competitors. We also have the <a href="<?php echo site_url(); ?>/warranty" target="_blank">best warranty</a> and customer service in the industry.</p>
+
+          <p>If you want to do business with a company that prides itself in answering the phone right away and values customer service over anything else . . . you are in the right place!</p>
+
+          <p><a href="<?php echo site_url(); ?>/contact" target="_blank">Call us today</a> and let us design, build and deliver a solution for you!</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section-difference">
+    <div class="container">
+      <img class="img-fluid d-block mx-auto" src="<?php echo get_template_directory_uri(); ?>/assets/img/what-sets-cjrtec-apart.png" alt="what sets cjrtec apart">
+
+      <h2 class="text-center mb-4">
+        What sets <span class="orange"><strong>CJR Tec</strong></span> apart?
+      </h2>
+
+      <ul class="text-center">
+        <li>
+          CJR currently enjoys a <strong>market share of nearly 70% of all machines
+          sold</strong> in USA.
+        </li>
+        <li>
+          CJR has <strong>over 40 machines in stock</strong> that can be shipped next day.
+        </li>
+        <li>
+          CJR has hundreds of <strong><em>American Clicker Presses</em></strong> in operation.
+        </li>
+        <li>CJR sells to <strong>Fortune 500 companies</strong> all over the world.</li>
+        <li>CJR has engineers that can support your after-sale service.</li>
+        <li>
+          CJR has an <a href="#clicker-press-parts" class="orange">extensive clicker press parts</a> department which offers
+          next day shipping for everything you need.
+        </li>
+        <li>
+          CJR offers the only <a href="<?php echo site_url(); ?>/warranty" target="_blank" class="orange">5-year warranty</a>&ast; in the industry.
+        </li>
+        <li>
+          CJR offers <a href="#tonnage-formula" class="orange"><strong>FREE</strong> tonnage testing</a> on any of our 40 machines in stock.
+        </li>
+        <li>
+          CJR offers a <a href="#clicker-press-parts" class="orange">buyback program</a> on all stock machines.
+        </li>
+        <li>
+          CJR offers a <a href="#finance" class="orange"><strong>FREE</strong> loaner program</a> on custom build machines.
+        </li>
+        <li>
+          CJR has <a href="<?php echo site_url(); ?>/customers" target="_blank" class="orange">hundreds of customers</a> that you are encouraged to talk to.
+        </li>
+        <li>
+          CJR engineers will visit your facility to discuss current needs for
+          FREE. All you do is pay for travel.
+        </li>
+      </ul>
+      
+      <p class="text-center text-muted">
+        <small>
+          &ast;All clicker presses have a FULL one-year warranty included
+          already. Extending the warranty to 5 years is optional, for an
+          additional fee.
+        </small>
+      </p>
+    </div>
+  </div>
+
+  <div class="cta-strip py-5">
+    <div class="container">
+      <h2>Create an Account with CJRTec Now</h2>
+      <a href="#registration" class="btn-cta-strip">
+        Get Started Today <i class="fa fa-angle-right"></i>
+      </a>
+    </div>
+  </div>
+  
+</section>
+
+<?php get_footer(); ?>
