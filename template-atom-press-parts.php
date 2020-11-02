42592c6b6a3fc0251d7904128b4e6964e9882ba2
Leo <leocsdev@gmail.com>
2020-11-02 11:56:58 +0800

    removed html markup from atom press parts page template and moved all content to wordpress page.

--- template-atom-press-parts.php
+++ template-atom-press-parts.php
@@ -10,58 +10,6 @@
   <div class="container">
     
     <?php the_content(); ?>
-    
-    
-    
-    
-    
-    
-    
-    <!-- <h1>Ordering Atom Clicker Press Parts</h1>
-    
-    <div class="row mb-5 d-flex">
-      <div class="col-md-7">
-        <p>Everyday, people contact us looking for parts for Atom presses. For the most part, these people are frustrated that they can’t find parts for these once great machines. Atom used to be the dominant provider of presses in the U.S. because factories trusted the Italian manufacturing. Atom’s older Italian presses were extremely well built but over the past decade they were replaced with cheap Chinese machines.</p>
-
-        <p>Most customers were completely unaware of this transition from Italian to Chinese machines and only discovered the issue after they had a machine go down and began searching for replacement parts. They learned the hard way that the older Italian machines were not compatible with the newer Chinese models.</p>
-
-        <a href="<?php // echo site_url(); ?>/china-steel" target="_blank">Click here to learn more about presses from China <i class="fa fa-chevron-right"></i></a>
-      </div>
-      
-      <div class="col-md-5 d-flex mt-lg-0">
-        <div class="img-responsive">
-          <img src="<?php // echo get_template_directory_uri(); ?>/assets/img/atom-press.png" alt="atom press">
-        </div>
-      </div>
-    </div>
-
-    <div class="row mb-5">
-      <div class="col-sm-12">
-        <p>Factories are now trying to rid themselves of the Italian Atom presses while they’re still running because they’ve learned that when they break, spare parts can’t be found to repair them. This is why there are so many Atom presses for sale. The smart money doesn’t want anything to do with them because the presses are worthless without replacement parts.</p>
-
-        <p>In an effort to be of service to our customers, we have worked hard to track down spare parts for the well-built, Italian presses. Locating usable parts has been a difficult process but we have finally been successful in finding some reliable sources – to get you back up and running should your press go down.</p>
-
-        <a href="#clicker-press-parts">Click here to view our clicker press parts <i class="fa fa-chevron-right"></i></a>
-      </div>
-    </div>
-
-    <div class="row mb-5">
-      <div class="col-sm-12">
-        <p>Then once your machine has been repaired, we recommend replacing it with a new one that has numerous sources for spare parts, lots of trained technicians that can work on it and a warranty.</p>
-
-        <a href="<?php // echo site_url(); ?>/warranty" target="_blank">Click here to read about our warranty <i class="fa fa-chevron-right"></i></a>
-      </div>
-    </div>
-
-    <div class="row mb-5">
-      <div class="col-sm-12">
-        <p>Unfortunately, the longer you stay with an older press, the greater your risk of ending up with a worthless machine that you can’t rely on.</p>
-
-        <p>Give us a call today and we’ll do what we can to be of help to you.</p>
-
-        <a href="#clicker-press">Click here to view our clicker press machines  <i class="fa fa-chevron-right"></i></a>
-      </div>
-    </div> -->
 
   </div>
 </section>
