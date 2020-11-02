773b7d81e7530537788df2b07d3e2a7a86a6b3f6
Leo <leocsdev@gmail.com>
2020-11-02 08:13:45 +0800

    Added code to display items by category.

--- template-accessories.php
+++ template-accessories.php
@@ -14,10 +14,15 @@
 
     <div class="mb-5"></div>
 
-    <!-- get items from CP Accessories CPT -->
+    <h2>SHEET FEEDERS</h2>
+    <!-- get items from CP Accessories CPT with Sheet Feeders category -->
     <div class="row mb-5">
       <?php 
-        $args = array('post_type' => 'cp_accessories');
+        $args = array(
+          'post_type' => 'cp_accessories',
+          'post_status' => 'publish',
+          'category_name' => 'Sheet Feeders' 
+      );
           $the_query = new WP_Query( $args );
       ?>
 
@@ -27,7 +32,6 @@
           $post = get_the_ID();
       ?>
       <div class="col-md-6 my-3"> 
-        
         <div class="card h-100 aos-init aos-animate" data-aos="fade-up">
           <img class="card-img-top" src="<?php echo the_field('image', $post); ?>" alt="card image">
           <div class="card-body">
@@ -47,7 +51,6 @@
             </p>
           </div>
         </div>
-        
       </div>
       <?php endwhile; ?>
       
@@ -56,6 +59,63 @@
       <?php else : ?>
         <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
       <?php endif; ?>
+    </div>
+  
+
+  
+
+  
+  
+  
+  
+    <h2>AIR ROLLS</h2>
+    <!-- get items from CP Accessories CPT with Air Rolls category -->
+    <div class="row mb-5">
+        <?php 
+          $args = array(
+            'post_type' => 'cp_accessories',
+            'post_status' => 'publish',
+            'category_name' => 'Air Rolls' 
+        );
+            $the_query = new WP_Query( $args );
+        ?>
+
+        <?php if ( $the_query->have_posts() ) : ?>
+        <?php 
+          while ( $the_query->have_posts() ) : $the_query->the_post();
+            $post = get_the_ID();
+        ?>
+        <div class="col-md-6 my-3"> 
+          
+          <div class="card h-100 aos-init aos-animate" data-aos="fade-up">
+            <img class="card-img-top" src="<?php echo the_field('image', $post); ?>" alt="card image">
+            <div class="card-body">
+              <h5 class="card-title">
+                <?php the_title(); ?>
+              </h5>
+              <p class="card-text">
+                Model: <?php echo the_field('model_number', $post) ?> <br>
+                Description: <?php echo the_field('description', $post) ?> <br>
+                Type: 
+                  <?php 
+                    $term = get_field('category_group'); 
+                    if ($term) {
+                      echo esc_html($term->name);
+                    }
+                  ?>
+              </p>
+            </div>
+          </div>
+          
+        </div>
+        <?php endwhile; ?>
+        
+        <?php wp_reset_query(); ?> 
+
+        <?php else : ?>
+          <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+        <?php endif; ?>
+      </div>
     </div>
   </div>
 </section>
