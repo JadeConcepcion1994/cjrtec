658ff872e8837becbf96ef7bc896a67ab67fd660
Leo <leocsdev@gmail.com>
2020-11-03 15:05:05 +0800

    updated content on types of dies page.

--- template-types-of-dies.php
+++ template-types-of-dies.php
@@ -11,88 +11,81 @@
 
     <?php the_content(); ?>
 
+    <!-- carousel -->
+    <div
+      id="carousel-testimonials"
+      class="carousel slide"
+      data-ride="carousel"
+    >
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img
+            class="d-block"
+            src="<?php bloginfo('template_directory'); ?>/assets/img/wesmar.png"
+            alt="First slide"
+          />
 
-    <!-- <h1 class="text-center">Types of Dies</h1>
-
-    <div class="row mb-5">
-      <div class="col-md-6 my-3">
-        <div class="card h-100 aos-init aos-animate" data-aos="fade-up">
-          
-          <img class="card-img-top" src="<?php // echo get_template_directory_uri(); ?>/assets/img/wood-backed-type-of-die.jpg" alt="Wood backed type of die.">
-          
-
-          <div class="card-body text-center">
-            <h2 class="card-title">
-              Wood-Backed Dies
-            </h2>
-            <p class="card-text">
-              There are many different type of dies that are commonly used with clicker presses. The most common is a wood-backed die with 927 steel rule.
+          <div class="testimonial-message">
+            <p class="description">
+              With CJR, you can find a nice assortment of top performing
+              Clicker Presses at prices that are substantially cheaper than
+              other brands.
             </p>
+            <h3>Kim Gerhart - <span>Wesmar Leather Goods</span></h3>
           </div>
         </div>
-      </div>
+        <div class="carousel-item">
+          <img
+            class="d-block"
+            src="<?php bloginfo('template_directory'); ?>/assets/img/us-duty-gear.png"
+            alt="Second slide"
+          />
 
-      <div class="col-md-6 my-3">
-        <div class="card h-100 aos-init aos-animate" data-aos="fade-up">
-          <img class="card-img-top" src="<?php // echo get_template_directory_uri(); ?>/assets/img/acrylic-backed-type-of-die.jpg" alt="Acrylic backed type of die.">
-
-          <div class="card-body text-center">
-            <h2 class="card-title ">
-              Acrylic-Backed Dies
-            </h2>
-            <p class="card-text">
-              Acrylic-backed dies are handy when you need to see through the die – in the event you need to place the die over a specific image or if you need to consider the grain or pattern of the material you are cutting.
+          <div class="testimonial-message">
+            <p class="description">
+              Our company is enjoying the benefits of our CJR automatic
+              cutting machine. It continuously improves our production and
+              labor practices.
             </p>
+            <h3>Jose Luis Flores - <span>US Duty Gear</span></h3>
           </div>
         </div>
-      </div>
-      
-      <div class="col-md-6 my-3">
-        <div class="card h-100 aos-init aos-animate" data-aos="fade-up">
-          <img class="card-img-top" src="<?php // cho get_template_directory_uri(); ?>/assets/img/steel-backed-type-of-die.jpg" alt="Steel backed type of die.">
+        <div class="carousel-item">
+          <img
+            class="d-block"
+            src="<?php bloginfo('template_directory'); ?>/assets/img/philly-rubber.png"
+            alt="Third slide"
+          />
 
-          <div class="card-body text-center">
-            <h2 class="card-title ">
-              Steel-Backed Dies
-            </h2>
-            <p class="card-text">
-              Some presses have magnetic heads that make for quick die changes that require metal backing. Some will simply add a thin, steel backing to their wood backed dies to accomplish the same thing. Sometimes steel-backed dies are necessary because of the specific needs of the die – such as spring ejection, heat-plate sealing or other considerations.
+          <div class="testimonial-message">
+            <p class="description">
+              While many factors always come into play in producing quality
+              products, CJR has helped us achieve these kinds of results.
             </p>
+            <h3>Jackie Hindman - <span>Philly Rubber</span></h3>
           </div>
         </div>
+
+        <a
+          class="carousel-control-prev"
+          href="#carousel-testimonials"
+          role="button"
+          data-slide="prev"
+        >
+          <span class="orange"><i class="fa fa-chevron-left"></i></span>
+        </a>
+
+        <a
+          class="carousel-control-next"
+          href="#carousel-testimonials"
+          role="button"
+          data-slide="next"
+        >
+          <span class="orange"><i class="fa fa-chevron-right"></i></span>
+        </a>
       </div>
-
-      <div class="col-md-6 my-3">
-        <div class="card h-100 aos-init aos-animate" data-aos="fade-up">
-          <img class="card-img-top" src="<?php // echo get_template_directory_uri(); ?>/assets/img/deep-steel-die.png" alt="Deep Steel die.">
-
-          <div class="card-body text-center">
-            <h2 class="card-title ">
-              Textile Dies
-            </h2>
-            <p class="card-text">
-              Textile dies actually go by a number of other names like envelope dies, deep steel dies, etc. These dies generally have no backing and are sometimes reinforced with metal runners if needed. You can see through these dies as well, which is helpful if registration is important or if you need to see the pattern of what you are cutting.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-6 my-3">
-        <div class="card h-100 aos-init aos-animate" data-aos="fade-up">
-          <img class="card-img-top" src="<?php // echo get_template_directory_uri(); ?>/assets/img/mold-type-of-die.jpg" alt="Mold type of die.">
-
-          <div class="card-body text-center">
-            <h2 class="card-title ">
-              Mold Dies
-            </h2>
-            <p class="card-text">
-              Mold dies are made up of a male and female component of the mold that works on a punch principle rather than a blade. This type of die is generally machined from various grades of steel or aluminum and assembled on two metal plates that are configured to cut in the same stroke position each time. This type of die can be designed with a number of functions like unique numbering stamps, heat plate sealing, spring ejection, registration pins, and so on.
-            </p>
-          </div>
-        </div>
-      </div>
-
-    </div> -->
+    </div><!-- carousel -->
+    
   </div>
 </section>
 
