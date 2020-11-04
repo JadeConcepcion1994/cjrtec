a2e2c624ec7368f4d7cef206e11d340b489d3b83
Leo <leocsdev@gmail.com>
2020-11-04 14:15:11 +0800

    Updated content for die maintenance page.

--- template-die-maintenance.php
+++ template-die-maintenance.php
@@ -8,47 +8,9 @@
 
 <section id="die-maintenance">
   <div class="container">
-    <h1 class="text-center">Die Maintenance and Repair Parts</h1>
-    
-    <div class="row mb-4 d-flex align-items-center">
-      <div class="col-md-4 d-none d-md-block">
-        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/die maintenance.png" alt="die maintenance">
-      </div>
-      <div class="col-md-8">
-        <p>If you’re new to the industry, this is an important concept to put into place – you need to implement some basic practices and procedures regarding your press and your press operators or you will end up with damaged dies. Dies need to be safe-guarded and maintained. Another critical element of press operation is material ejection – which is a bit of an art that can save you hundreds of hours.</p>
 
-        <p>If you have one part of the die where the product is getting stuck, teach your people to be mindful of the problem and take a moment to fix it by adding or moving ejection foam. If your hole punches are getting clogged and no one takes time to clear them, they will eventually blow out the size of the hole and even split the punch to where it&apos;s not cutting any more. If you get a product that has holes in it, you will want to have spare hole punches so the dull punches can be replaced without rendering the entire die useless.</p>
-      </div>
-    </div>
+    <?php the_content(); ?>
 
-    <p>
-      We offer training at our facility in Utah and can even come to your location if you would like. <br>
-      
-      <a href="#training">
-        Click here to learn more about our training 
-        <i class="fa fa-chevron-right"></i>
-      </a>
-    </p>
-
-    <p>
-      We have also produced a number of valuable videos to help train new press owners and operators. <br>
-
-      <a href="<?php echo site_url(); ?>/videos" target="_blank">
-        Click here to see our videos 
-        <i class="fa fa-chevron-right"></i>
-      </a>
-    </p>
-
-    <p>
-      If you find that part of your die is not cutting anymore, you can take the die to your local die maker and request that they replace some of the rule or you can buy a steel rule sharpener and put a new edge on what you have. A little preventative maintenance will go a long way. <br>
-
-      <a href="<?php echo site_url(); ?>/die-makers" target="_blank">
-        Click here to view die makers around the country 
-        <i class="fa fa-chevron-right"></i>
-      </a>
-    </p>
-
-    <p class="mb-5">These details and many more work together to affect production and cost per piece. If your workers are not taught what to look for or trained how to be vigilant, you won’t be able to count on them as useful components of your production process.</p>
   </div>
 </section>
 
