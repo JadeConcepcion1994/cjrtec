af8d91b7e428397675123ec59a5dc879baa4c71d
Leo <leocsdev@gmail.com>
2020-11-04 11:01:45 +0800

    Updated registration pins page template.

--- template-registration-pins.php
+++ template-registration-pins.php
@@ -8,23 +8,9 @@
 
 <section id="registration-pins">
   <div class="container">
-    <h1 class="text-center">Registration Pins</h1>
+    
+    <?php the_content(); ?>
 
-    <div class="row mb-5">
-      <div class="col-md-5">
-        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/registration pins.jpg" alt="Registration pins">
-
-        <p class="mt-4 mb-5">Registration pins are used for aligning printed material so you're cutting in the right place every time. The pins are spring backed so they can be pushed down without damaging the press or die.</p>
-      </div>
-
-      <div class="col-md-7">
-        <div class="embed-responsive embed-responsive-16by9">
-          <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/_5sprjhXBfo" allowfullscreen></iframe>
-        </div>
-
-        <h4 class="text-center mt-3">Die Alignment Pins</h4>
-      </div>
-    </div>
   </div>
 </section>
 
