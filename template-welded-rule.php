7ba6997e8f3a6c914967c83f74b71ecec7c1b415
Leo <leocsdev@gmail.com>
2020-11-04 12:04:43 +0800

    Updated content for welded rule page.

--- template-welded-rule.php
+++ template-welded-rule.php
@@ -8,16 +8,76 @@
 
 <section id="welded-rule">
   <div class="container">
-    <div class="section-content mb-5">
-      <h1 class="text-center">Welded Rule</h1>
 
-      <p>Although it increases the cost of the die, welding the connecting points of your steel rule will give you a better cut and oftentimes make your die last longer. Every time you make a cut, you are putting pressure against the sides of the die. Depending on how much pressure is exerted, the ejected material will eventually press against the sides enough that a gap will begin to develop. If you get a gap between two ends of the rule, your material will no longer cut in that area.</p>
+    <?php the_content(); ?>
 
-      <p>If you are cutting with an automatic press or CNC press, you should always request welded rule because it is stronger and less susceptible to pulling out of the wood or acrylic backing. Over time, the steel rule will work its way loose to the point that it can pull out of its backing. If this happens in automatic mode, the rule can cut a die in half or damage your material.</p>
+  </div>
 
-      <p>After the die maker welds the edges, they will grind the blade again to give it a sharp edge. When edges are welded, you have a stronger and safer die.</p>
+  <div class="section-difference">
+    <div class="container">
+      <img class="img-fluid d-block mx-auto" src="<?php echo get_template_directory_uri(); ?>/assets/img/what-sets-cjrtec-apart.png" alt="what sets cjrtec apart">
+
+      <h2 class="text-center mb-4">
+        What sets <span class="orange"><strong>CJR Tec</strong></span> apart?
+      </h2>
+
+      <ul class="text-center">
+        <li>
+          CJR currently enjoys a <strong>market share of nearly 70% of all machines
+          sold</strong> in USA.
+        </li>
+        <li>
+          CJR has <strong>over 40 machines in stock</strong> that can be shipped next day.
+        </li>
+        <li>
+          CJR has hundreds of <strong><em>American Clicker Presses</em></strong> in operation.
+        </li>
+        <li>CJR sells to <strong>Fortune 500 companies</strong> all over the world.</li>
+        <li>CJR has engineers that can support your after-sale service.</li>
+        <li>
+          CJR has an <a href="#clicker-press-parts" class="orange">extensive clicker press parts</a> department which offers
+          next day shipping for everything you need.
+        </li>
+        <li>
+          CJR offers the only <a href="<?php echo site_url(); ?>/warranty" target="_blank" class="orange">5-year warranty</a>&ast; in the industry.
+        </li>
+        <li>
+          CJR offers <a href="#tonnage-formula" class="orange"><strong>FREE</strong> tonnage testing</a> on any of our 40 machines in stock.
+        </li>
+        <li>
+          CJR offers a <a href="#clicker-press-parts" class="orange">buyback program</a> on all stock machines.
+        </li>
+        <li>
+          CJR offers a <a href="#finance" class="orange"><strong>FREE</strong> loaner program</a> on custom build machines.
+        </li>
+        <li>
+          CJR has <a href="<?php echo site_url(); ?>/customers" target="_blank" class="orange">hundreds of customers</a> that you are encouraged to talk to.
+        </li>
+        <li>
+          CJR engineers will visit your facility to discuss current needs for
+          FREE. All you do is pay for travel.
+        </li>
+      </ul>
+      
+      <p class="text-center text-muted">
+        <small>
+          &ast;All clicker presses have a FULL one-year warranty included
+          already. Extending the warranty to 5 years is optional, for an
+          additional fee.
+        </small>
+      </p>
     </div>
   </div>
+
+  <div class="cta-strip py-5">
+    <div class="container">
+      <h2>Create an Account with CJRTec Now</h2>
+      <a href="#registration" class="btn-cta-strip">
+        Get Started Today <i class="fa fa-angle-right"></i>
+      </a>
+    </div>
+  </div>
+  
 </section>
 
 <?php get_footer(); ?>
