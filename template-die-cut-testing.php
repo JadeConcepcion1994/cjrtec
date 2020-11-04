6e5ef64fa4fabdc42dd464b5cacf66716e19c48d
Leo <leocsdev@gmail.com>
2020-11-04 14:52:35 +0800

    Updated content for die cut testing page.

--- template-die-cut-testing.php
+++ template-die-cut-testing.php
@@ -8,68 +8,9 @@
 
 <section id="die-cut-testing">
   <div class="container">
-    <h1 class="text-center" >Die Cut Testing</h1>
 
-    <div class="press-size mb-5">
-      <h2>What press size do you need?</h2>
+    <?php the_content(); ?>
 
-      <p>If you want to be sure that a specific press will cut your material with a specific die, the quickest and easiest way is to send us your die and material so we can perform some tests for you.</p>
-
-      <p>We can test your die and material on a variety of our machines to verify that the tonnage you are requesting is adequate.</p>
-
-      <p>We video the whole process and then upload the videos for your review.</p>
-
-      <a href="#diagnostics">
-        Click here to learn more about our diagnostics 
-        <i class="fa fa-chevron-right"></i>
-      </a>
-    </div>
-
-    <div class="what-to-send mb-5">
-      <h2>What to send?</h2>
-
-      <p>If you want us to perform these tests for you, please send enough material to test on more than one press. Understand that to test a die, we need to place the die and material and start off with small amounts of pressure, incrementally increasing it until the die cuts all the way through with one stroke. This process results in the loss of some material. If we cut once or twice and only get partially through the material we are unable to use the same material for the next test because its already partially cut. If we discover that the first press doesn&apos;t have enough pressure, we will need to start the process all over on another press.</p>
-
-      <p>This testing takes a little time, but once you know where you&apos;re at and record the settings, you don&apos;t have to do it again.</p>
-
-      <p>Our purpose for explaining this is to help you understand the process, so you send enough material to complete the test.</p>
-    </div>
-
-    <div class="testing-fee mb-5">
-      <h2>Testing Fee</h2>
-
-      <p><u>Test cut fees are returned as a credit to your purchase</u></p>
-
-      <p>Most of our competition are re-selling Chinese machines, and don&apos;t have in-stock machines of their own – so they have no way of performing tests on anything bigger than a small swing arm press. For nearly 5 years, our testing has been a free service. Unfortunately, there have been numerous instances where our service has been abused – changing our policy for everyone. Some of our competitors encouraged their prospective buyers to use our free testing service, then using that data to spec their own machine.</p>
-
-      <p>We have over 20 presses on our floor from 10 ton to 100 ton, in a myriad of different sizes. This is a significant investment that is a necessary part of doing business the right way.</p>
-
-      <p><strong>We offer test cutting for &dollar;200.00</strong> — which covers the cost to make a video and upload it online for review. This also covers the cost of re-packaging your die for return. Just to be clear, our test cutting fee are returned to you as a credit on your purchase of a press from us.</p>
-
-      <a href="#cjrterms">Read our terms and policies 
-        <i class="fa fa-chevron-right"></i>
-      </a>
-    </div>
-
-    <div class="mailing-address mb-5">
-      <h2>Mailing Address</h2>
-
-      <p>If you&apos;re going to be sending us anything, please notify our office in advance so we can be looking for the delivery. Make sure we have your complete contact information as well as a note indicating what type of press you would like us to test.Please include a return shipping label to be used once we complete your testing.</p>
-
-      <p>Send your samples to:</p>
-
-      <p>
-        <strong>CJR Distributing</strong> <br>
-        7023 S 700 W Building B <br>
-        Midvale UT 84047 <br>
-        USA
-      </p>
-
-      <a href="#contact">
-        Call us or send us an email 
-        <i class="fa fa-chevron-right"></i>
-      </a>
-    </div>
   </div>
 </section>
 
