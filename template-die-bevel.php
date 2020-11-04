d41a20b2b8f989b7cd233bd9f2103d2a14ba29e6
Leo <leocsdev@gmail.com>
2020-11-04 10:19:18 +0800

    Updated die bevel page template.

--- template-die-bevel.php
+++ template-die-bevel.php
@@ -8,20 +8,9 @@
 
 <section id="die-bevel" class="mb-5">
   <div class="container">
-    <h1 class="text-center">Die Bevel</h1>
     
-    <div class="img-container">
-      <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/different-die-bevel.jpg" alt="Different Types of Steel Rule Die Bevels">
-    </div>
-
-    <p class="mt-5">The bevel on steel rule is the angled part of the blade that makes up the sharp cutting edge. The bevel has a lot to do with the type of cut, positioning of the cut, and accuracy of the cut. The longer the bevel, for example, the easier it is for the blade to move through your material, but the blade life will not be as long. You also have a choice of a center bevel and a side bevel. The bevel you choose is generally based on the type of material you are cutting, and whether you are cutting one layer or multiple layers at a time.</p>
-
-    <p>This is important because if you have a die that is close to requiring the maximum pressure your press has to offer, you can change the bevel length of your die, and sometimes cut more materials with less pressure.</p>
-
-    <a href="#steel-rule">
-      Click here to learn more about steel rule  
-      <i class="fa fa-chevron-right"></i>
-    </a>
+    <?php the_content(); ?>
+    
   </div>
 </section>
 
