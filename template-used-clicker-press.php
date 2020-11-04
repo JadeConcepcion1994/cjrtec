57a4cd97214bf05eae20fae3f815c32080bea506
Leo <leocsdev@gmail.com>
2020-11-04 16:15:26 +0800

    Updated content for used clicker press page.

--- template-used-clicker-press.php
+++ template-used-clicker-press.php
@@ -7,62 +7,66 @@
 <?php get_header(); ?>
 
 <section id="used-clicker-press">
-  <div class="info-1 mb-5">
-    <div class="container">
-      <h1>Used Clicker Presses</h1>
-
-      <div class="row mb-5 d-flex">
-        <div class="col-md-5 d-flex mb-lg-0">
-          <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/used-clicker-press.png" alt="Used Clicker Press">
-        </div>
-
-        <div class="col-md-7">
-          <p>Over 80&percnt; of the used machines in the market are discontinued from being manufactured — which means that replacement parts for these machines is hard, or worse, cannot be obtainable anymore. We realize that there may be some good used machines available but if you can't get parts, it's just a matter of time before an expensive investment becomes worthless.</p>
-
-          <p>Quite frankly, this is why there are so many tired and worn out machines on the market. Large manufacturers are dumping their older presses before it's too late and instead are investing in machines with warranties and available parts. They don't want to be the ones holding the bag when an old press finally breaks down.</p>
-
-          <p>If you've been looking at used machines, insist on getting the contact information of the actual manufacturer and call them to verify that they are still in business and is willing to send service information and parts in the event of a break down. Then you should ask the seller to give you a written one-year warranty for your protection.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="info-2 py-5 my-5">
-    <div class="container">
-      <img class="img-fluid d-block mb-4 text-center" src="<?php echo get_template_directory_uri(); ?>/assets/img/stay-informed-green.png" alt="Stay informed green">
-
-      <h2 class="text-center mb-4">Be Informed and Be Careful</h2>
-
-      <p class="text-center">We stopped accepting trade-ins in purchasing our new machines because most of the used machines in the market are no longer being manufactured, which means that their parts are no longer available. After getting stuck with our share of used presses, we made the decision that it’s no longer worth the risk.</p>
-    </div>
-  </div>
-
-  <div class="row cjrtec-difference" style="margin-top: 50px; color: black; margin-bottom: 50px">
- 		<div class="col-md-12">
- 			<center><img src="<?php echo get_template_directory_uri(). "/assets/img/what-sets-cjrtec-apart.png" ?>" class="img-responsive" alt="what sets cjrtec apart from other companies">
-
- 			<h2>What sets <span class="font-weight-bolder orange">CJR Tec</span> apart?</h2>
- 			<br>
-		        <ul id="ul_check">
-					<li>CJR currently enjoys a <b>market share of nearly 70% of all machines sold</b> in USA. </li>
-					<li>CJR has <b>over 40 machines in stock</b> that can be shipped next day. </li>
-					<li>CJR has hundreds of <em style="font-size: 25px;">American Clicker Presses</em> in operation. </li>
-					<li>CJR sells to <b>Fortune 500 companies</b> all over the world.  </li>
-					<li>CJR has engineers that can support your after-sale service.</li>
-					<li>CJR has an <a href="https://www.cjrtec.com/clicker-press-parts.php" class="link">extensive clicker press parts</a> department which offers next day shipping for everything you need.  </li>
-					<li>CJR offers the only <a href="https://www.cjrtec.com/warranty.php" class="link">5-year warranty</a>* in the industry.</li>
-					<li>CJR offers <a href="https://www.cjrtec.com/tonnage-formula.php" class="link"><b>FREE</b> tonnage testing</a> on any of our 40 machines in stock. </li>
-					<li>CJR offers a <a href="https://www.cjrtec.com/clicker-press-parts.php" class="link">buyback program</a> on all stock machines.</li>
-					<li>CJR offers a <a href="https://www.cjrtec.com/finance.php" class="link"><b>FREE</b> loaner program</a> on custom build machines.</li>
-					<li>CJR has <a href="https://www.cjrtec.com/customers.php" class="link">hundreds of customers</a> that you are encouraged to talk to. </li> 
-					<li>CJR engineers will visit your facility to discuss current needs for <b>FREE</b>. All you do is pay for travel.</li> 
-				</ul><br>
-			</center>
-			<center><small> * All clicker presses have a FULL one-year warranty included already. Extending the warranty to 5 years is optional, for an additional fee.</small><br/></center>
- 		</div>
-	</div>
   
-  <div class="cta-strip py-5">
+  <?php the_content(); ?>
+
+  <div class="section-difference">
+    <div class="container">
+      <img class="img-fluid d-block mx-auto" src="<?php echo get_template_directory_uri(); ?>/assets/img/what-sets-cjrtec-apart.png" alt="what sets cjrtec apart">
+
+      <h2 class="text-center mb-4">
+        What sets <span class="orange"><strong>CJR Tec</strong></span> apart?
+      </h2>
+
+      <ul class="text-center">
+        <li>
+          CJR currently enjoys a <strong>market share of nearly 70% of all machines
+          sold</strong> in USA.
+        </li>
+        <li>
+          CJR has <strong>over 40 machines in stock</strong> that can be shipped next day.
+        </li>
+        <li>
+          CJR has hundreds of <strong><em>American Clicker Presses</em></strong> in operation.
+        </li>
+        <li>CJR sells to <strong>Fortune 500 companies</strong> all over the world.</li>
+        <li>CJR has engineers that can support your after-sale service.</li>
+        <li>
+          CJR has an <a href="#clicker-press-parts" class="orange">extensive clicker press parts</a> department which offers
+          next day shipping for everything you need.
+        </li>
+        <li>
+          CJR offers the only <a href="<?php echo site_url(); ?>/warranty" target="_blank" class="orange">5-year warranty</a>&ast; in the industry.
+        </li>
+        <li>
+          CJR offers <a href="#tonnage-formula" class="orange"><strong>FREE</strong> tonnage testing</a> on any of our 40 machines in stock.
+        </li>
+        <li>
+          CJR offers a <a href="#clicker-press-parts" class="orange">buyback program</a> on all stock machines.
+        </li>
+        <li>
+          CJR offers a <a href="#finance" class="orange"><strong>FREE</strong> loaner program</a> on custom build machines.
+        </li>
+        <li>
+          CJR has <a href="<?php echo site_url(); ?>/customers" target="_blank" class="orange">hundreds of customers</a> that you are encouraged to talk to.
+        </li>
+        <li>
+          CJR engineers will visit your facility to discuss current needs for
+          FREE. All you do is pay for travel.
+        </li>
+      </ul>
+      
+      <p class="text-center text-muted">
+        <small>
+          &ast;All clicker presses have a FULL one-year warranty included
+          already. Extending the warranty to 5 years is optional, for an
+          additional fee.
+        </small>
+      </p>
+    </div>
+  </div>
+  
+  <div class="cta-strip">
     <div class="container">
       <h2>Create an Account with CJRTec Now</h2>
       <a href="#registration" class="btn-cta-strip">
