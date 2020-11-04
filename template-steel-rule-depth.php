d65998c2abd29c50b8ef7a137a403e5aca7c7f2a
Leo <leocsdev@gmail.com>
2020-11-04 11:37:41 +0800

    Updated content for steel rule depth page template.

--- template-steel-rule-depth.php
+++ template-steel-rule-depth.php
@@ -8,30 +8,10 @@
 
 <section id="steel-rule-depth">
   <div class="container">
-    <div class="section-content mb-5">
-      <h1 class="text-center">Steel Rule Depth</h1>
 
-      <p>Sufficient die depth is essential if you are cutting tall material. Most materials will compress when being cut, but you can only compress so far before you begin to damage your die or material. The average wood backed die with .937 steel rule will have about a 1/4&quot; cavity. However, you have to account for your ejection foam, which limits cavity depth as well.</p>
+    <?php the_content(); ?>
 
-      <p>If you have a 1/4&quot; cavity, you can&apos;t cut a 1/4&quot; of paper or plastic because there is no place for the material to go. If you want to cut more material and you have enough pressure to do so, you will need to change your rule height so your product has someplace to go during the cut.</p>
-    </div>
-
-    <div class="row my-5">
-      <div class="col-md-4">
-        <img class="img-fluid d-block mx-auto mb-4" src="<?php echo get_template_directory_uri(); ?>/assets/img/steel-rule-depth-1.jpg" alt="Steel Rule Depth-Small">
-      </div>
-
-      <div class="col-md-4">
-        <img class="img-fluid d-block mx-auto mb-4" src="<?php echo get_template_directory_uri(); ?>/assets/img/steel-rule-depth-2.jpg" alt="Steel Rule Depth-Medium">
-      </div>
-      
-      <div class="col-md-4">
-        <img class="img-fluid d-block mx-auto mb-4" src="<?php echo get_template_directory_uri(); ?>/assets/img/steel-rule-depth-3.jpg" alt="Steel Rule Depth-Large">
-      </div>
-    </div>
   </div>
-
-
 </section>
 
 <?php get_footer(); ?>
