6320a1cc394934b08b88165aa3bca280ad583454
Leo <leocsdev@gmail.com>
2020-11-05 13:31:08 +0800

    Added die consideration page template.

---
+++ template-die-consideration.php
@@ -0,0 +1,17 @@
+<?php
+  /*
+  Template Name: Die Consideration
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="die-consideration">
+  <div class="container">
+    
+    <?php the_content(); ?>
+
+  </div>
+</section>
+
+<?php get_footer(); ?>
