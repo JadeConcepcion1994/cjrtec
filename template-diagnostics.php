b35af7adf7e2f8e7a7280424375897cf466de6a7
Leo <leocsdev@gmail.com>
2020-11-05 13:45:12 +0800

    Added diagnostics page template.

---
+++ template-diagnostics.php
@@ -0,0 +1,17 @@
+<?php
+  /*
+  Template Name: Diagnostics
+  */
+?>
+
+<?php get_header(); ?>
+
+<section id="diagnostics">
+  <div class="container">
+    
+    <?php the_content(); ?>
+
+  </div>
+</section>
+
+<?php get_footer(); ?>
