0c8285f3cef2e0686ddd4318b23ccbc8d2402634
Leo <leocsdev@gmail.com>
2020-11-05 16:19:22 +0800

    Commented out sections in front-page.php

--- front-page.php
+++ front-page.php
@@ -1,142 +1,164 @@
 
-<?php 
-get_header();
-?>
+<?php get_header(); ?>
 	
 	<div class="container-fluid cntr-top-content">
-		<?php  include get_theme_file_path( '/inc/inc.banner_carousel.php' ); ?>
-	
-		 <div class="row">
-		 <?php  include get_theme_file_path( '/inc/inc.header_second.php' ); ?>
-		 </div>
-
-		 <div class="container">
-	     	 <?php  include get_theme_file_path( '/inc/inc.know_the_difference.php' ); ?>
-		 </div>
+		<!-- <div class="row">
+			<?php // include get_theme_file_path( '/inc/inc.banner_carousel.php' ); ?>
+		</div> -->
+			
 		
-		 <div data-aos="fade-up" data-aos-delay="50">
-		 	<div class="row row-numbers">
-		 		<div class="col-md-3">
-		 			<div class="row">
-		 				<h1 class="Count">19</h1>
-		 				<h5 class="h5-number-title"><strong>YEARS OF <br> EXPERIENCED</strong></h5>
-		 			</div>	
-		 		</div>
-		 		<div class="col-md-3">
-		 			<div class="row">
-		 				<h1 class="Count">966</h1>
-		 				<h5 class="h5-number-title"><strong>PROJECT <br> SUCCESSFULL</strong></h5>
-		 			</div>	
-		 		</div>
-		 		<div class="col-md-3">
-		 			<div class="row">
-		 				<h1 class="Count">64</h1>
-		 				<h5 class="h5-number-title"><strong>PROFESSIONAL <br> EXPERT</strong></h5>
-		 			</div>	
-		 		</div>
-		 		<div class="col-md-3">
-		 			<div class="row">
-		 				<h1 class="Count">193</h1>
-		 				<h5 class="h5-number-title"><strong>HAPPY <br> CUSTOMERS</strong></h5>
-		 			</div>	
-		 		</div>
-		 	</div>
-		 </div>
+		<!-- <div class="row">
+			<?php // include get_theme_file_path( '/inc/inc.header_second.php' ); ?>
+		</div> -->
+
+		<!-- <div class="container">
+				<?php // include get_theme_file_path( '/inc/inc.know_the_difference.php' ); ?>
+		</div> -->
+
+
+		
+		<!-- <div data-aos="fade-up" data-aos-delay="50">
+			<div class="row row-numbers">
+				<div class="col-md-3">
+					<div class="row">
+						<h1 class="Count">19</h1>
+						<h5 class="h5-number-title"><strong>YEARS OF <br> EXPERIENCED</strong></h5>
+					</div>	
+				</div>
+				<div class="col-md-3">
+					<div class="row">
+						<h1 class="Count">966</h1>
+						<h5 class="h5-number-title"><strong>PROJECT <br> SUCCESSFULL</strong></h5>
+					</div>	
+				</div>
+				<div class="col-md-3">
+					<div class="row">
+						<h1 class="Count">64</h1>
+						<h5 class="h5-number-title"><strong>PROFESSIONAL <br> EXPERT</strong></h5>
+					</div>	
+				</div>
+				<div class="col-md-3">
+					<div class="row">
+						<h1 class="Count">193</h1>
+						<h5 class="h5-number-title"><strong>HAPPY <br> CUSTOMERS</strong></h5>
+					</div>	
+				</div>
+			</div>
+		</div> -->
+		
+
+
+		<!-- <div class="row">
+			<div class="col-md-12" data-aos="fade-up" data-aos-delay="50">
+				<h1 class="row-title-h1">
+					<?php 
+						// if(get_theme_mod('cjrtec_cards_title_edit')){
+						// 	echo get_theme_mod('cjrtec_cards_title_edit');
+						// }else{
+						// 	echo "Our Bestsellers";
+						// }
+					?>
+				</h1>
+			</div>
+		</div> -->
+
+
+
+		<!-- <div class="container-fluid cnt-flu">
+
+			<?php // include get_theme_file_path( '/inc/inc.best_sellers.php' ); ?>
+
+			<br>
+
+			<div class="row">
+				<div class="col-md-12">
+					<div class="zoom-wrap">
+						<img src="
+							<?php 
+								// if(wp_get_attachment_urL(get_theme_mod('cjrtec_lower_main_image')) ){
+								// 	echo wp_get_attachment_urL(get_theme_mod('cjrtec_lower_main_image'));
+								// }else{
+								// 	echo get_template_directory_uri(). "/assets/img/pick-and-place-clicker-press.jpg";
+								// }
+							?>
+						">
+			
+						<div class="centered">
+							<div data-aos="fade-up" data-aos-delay="50">
+								<h3 class="centered-text">
+									<strong>
+										<?php 
+											// if(get_theme_mod('cjrtec_lower_title_edit')){
+											// 	echo get_theme_mod('cjrtec_lower_title_edit');	
+											// }else{
+											// 	echo "Why Choose Clicker Press by CJRTec?";
+											// }
+										?>
+									</strong>
+								</h3>
+								
+								<p>
+									There are other companies out there but we are confident that nobody else can give you the BEST deals in the market
+								</p>
+							</div>
+						</div>
+					</div>	 	
+				</div>	
+			</div>
+
+			<div class="container-fluid">
+				<?php // include get_theme_file_path( '/inc/inc.why_choose.php' ); ?>
+			</div>
+
+		</div> -->
 		
 		
-
-
-		 <div class="row">
-		 	<div class="col-md-12" data-aos="fade-up" data-aos-delay="50">
-		 		<h1 class="row-title-h1">
-		 			<?php 
-		 			if(get_theme_mod('cjrtec_cards_title_edit')){
-		 			  echo get_theme_mod('cjrtec_cards_title_edit');
-		 			}else{
-		 			  echo "Our Bestsellers";
-		 			}
-		 			 
-		 			?>
-		 			</h1>
-		 	</div>
-		 </div>
-
-		 <div class="container-fluid cnt-flu">
-		 <?php  include get_theme_file_path( '/inc/inc.best_sellers.php' ); ?>
-
-		 	<br>
-		 	<div class="row">
-		 		<div class="col-md-12">
-		 		<div class="zoom-wrap">
-		 		
-						<img src="<?php 
-						if(wp_get_attachment_urL(get_theme_mod('cjrtec_lower_main_image')) ){
-							echo wp_get_attachment_urL(get_theme_mod('cjrtec_lower_main_image'));
-						}else{
-							echo get_template_directory_uri(). "/assets/img/pick-and-place-clicker-press.jpg";
-						}
-						
-
-						?>">
 		
-		 			<div class="centered">
-		 				<div data-aos="fade-up" data-aos-delay="50">
-		 				<h3 class="centered-text"><strong>
-		 					<?php 
-		 						if(get_theme_mod('cjrtec_lower_title_edit')){
-		 							echo get_theme_mod('cjrtec_lower_title_edit');	
-		 						}else{
-		 							echo "Why Choose Clicker Press by CJRTec?";
-		 						}
+		<!-- <br> -->
 
-		 					?>
-		 				</strong></h3>
-		 				<p>
-		 					There are other companies out there but we are confident that nobody else can give you the BEST deals in the market
-		 				</p>
-		 				</div>
-		 			</div>
-		 		</div>	 	
-		 		</div>	
-		 	</div>
-		 	<div class="container-fluid">
-			<?php  include get_theme_file_path( '/inc/inc.why_choose.php' ); ?>
-		 	</div>
-		 </div><br>
 
-		 <div class="container" style="color: black; margin-top: 50px">
-		 	<?php  include get_theme_file_path( '/inc/inc.get_help.php' ); ?>
-		 </div>
 
-		 <div class="row" style="margin-bottom: 50px">
-		 	<div class="col-md-12">
-		 		<div data-aos="fade-down" data-aos-delay="510">
-		 		<h2 class="row-title-h1">See what people are saying about <strong style="color:rgb(255, 88, 41)">CJRTec</strong></h2>	
-		 		<p align="center" style="color: black">
-		 			Our customers are our biggest assets. Take a look at what they say about our machines and services.
-		 		</p>
-		 		<p align="center">
-		 			<a href="" style="margin-right: 10px">View Our Customers <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
-		 			<a href="">Read More <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
-		 		</p>
-		 		</div>
-		 	</div>
-		 </div>
-		 <div class="row" style="margin-bottom: 100px">
-		 	<?php  include get_theme_file_path( '/inc/inc.cjr_testimonials.php' ); ?>
-		 </div>
+		<!-- <div class="container" style="color: black; margin-top: 50px">
+			<?php // include get_theme_file_path( '/inc/inc.get_help.php' ); ?>
+		</div> -->
 
-		<div data-aos="fade-up" data-aos-delay="510">
-			<?php  include get_theme_file_path( '/inc/inc.client_carousel.php' ); ?>
-		</div>
+		
 
-		<div class="row">
-			<?php  include get_theme_file_path( '/inc/inc.take_advantage.php' ); ?>
-		</div>
+		<!-- <div class="row" style="margin-bottom: 50px">
+			<div class="col-md-12">
+				<div data-aos="fade-down" data-aos-delay="510">
+					<h2 class="row-title-h1">
+						See what people are saying about <strong style="color:rgb(255, 88, 41)">CJRTec</strong>
+					</h2>	
+					<p align="center" style="color: black">
+						Our customers are our biggest assets. Take a look at what they say about our machines and services.
+					</p>
+					<p align="center">
+						<a href="" style="margin-right: 10px">View Our Customers <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+						<a href="">Read More <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+					</p>
+				</div>
+			</div>
+		</div> -->
 
-	</div>
-  </div>
 
-<?php 
-get_footer();
-?>
+
+		<!-- <div class="row" style="margin-bottom: 100px">
+			<?php // include get_theme_file_path( '/inc/inc.cjr_testimonials.php' ); ?>
+		</div> -->
+
+
+
+		<!-- <div data-aos="fade-up" data-aos-delay="510">
+			<?php // include get_theme_file_path( '/inc/inc.client_carousel.php' ); ?>
+		</div> -->
+
+
+
+		<!-- <div class="row">
+			<?php // include get_theme_file_path( '/inc/inc.take_advantage.php' ); ?>
+		</div> -->
+
+	</div> <!-- container-fluid cntr-top-content -->
+
+<?php get_footer(); ?>
