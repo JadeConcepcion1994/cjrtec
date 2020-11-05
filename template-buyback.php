aa4fbb9cc2df6f62e6a8a6e6108cffae3121de4e
Leo <leocsdev@gmail.com>
2020-11-05 13:00:06 +0800

    Updated content for buyback page.

--- template-buyback.php
+++ template-buyback.php
@@ -6,60 +6,11 @@
 
 <?php get_header(); ?>
 
-<section id="buyback" class="py-5">
+<section id="buyback">
   <div class="container">
-    <h1 class="text-center mb-5">
-      Buy Back Protection On All Clicker Presses and Cutting Machines
-    </h1>
 
-    <div class="section-image float-lg-right px-4">
-      <img class="img-fluid d-block mx-auto mb-5 mb-lg-1" src="<?php echo get_template_directory_uri(); ?>/assets/img/buyback.png" alt="buyback protection">
-    </div>
+    <?php the_content(); ?>
 
-    <div class="section-content">
-      <p>One unique offering that you&apos;ll only find at CJR is our <strong>Buy Back Program.</strong></p>
-
-      <p>On the surface, most customers may not appreciate the value of this offering, but once they understand the advantage of the offering, they don&apos;t want to buy from anyone else.</p>
-
-      <p>Essentially, CJR guarantees to buy back any press up to 40 tons which is purchased within the first 6 months for 90&percnt; of the original purchase price. Machines that are being traded in with 12 months are bought back for 80&percnt; of the purchase price.</p>
-
-      <p>To understand why this is such a big deal, you first need to understand the state of the industry. Today, hundreds of factories everywhere have discovered that they are no longer able to get parts or service on their clicker presses. The reason for this is that 80&percnt; of all manufacturers have gone out of business over the past 10 years because of all the cheap Chinese machines that have hit the market.</p>
-
-      <p class="mb-5">
-        <a href="<?php echo site_url(); ?>/china-steel" target="_blank">
-          Learn more about China presses <i class="fa fa-chevron-right"></i>
-        </a>
-      </p>
-
-      <p>Consequently, they are trying to dump their machines while they are still running because they realize that once their machine breaks, it&apos;s going to be a boat anchor. They want to sell it while they can to any unsuspecting buyer that isn&apos;t aware that the manufacturer is out of business.</p>
-      
-      <p>We have one factory that is dumping over 600 used machines and replacing them with new machines with warranty, parts and service.</p>
-
-      <p class="mb-5">
-        <a href="<?php echo site_url(); ?>/used-clicker-press" target="_blank">
-          Learn more about used press machines 
-          <i class="fa fa-chevron-right"></i> 
-        </a>
-
-        <br>
-        
-        <a href="#clicke-press-parts">
-          Shop clicker press parts here <i class="fa fa-chevron-right"></i>
-        </a>
-      </p>
-
-      <p>With so many machines in the second-hand market, prices have depreciated which means the last thing you want to do is sell a recently purchased machine where you will get seriously hurt.</p>
-
-      <p>CJR works with their customers in a way that allows them to buy what they need today without the fear of losing big if they need to jump up to a larger machine in the future.</p>
-
-      <p>No one else offers a program that allows customers to expand their business, increase production and reduce costs without the risk of big equipment losses.</p>
-
-      <p>
-        <a href="<?php echo site_url(); ?>/about">
-          Know more about us <i class="fa fa-chevron-right"></i>
-        </a>
-      </p>
-    </div>
   </div>
 </section>
 
