1fcac286af5734756f5c2beeec5b852482612a49
Leo <leocsdev@gmail.com>
2020-11-11 13:32:51 +0800

    Uncomment sections in frontpage.

--- front-page.php
+++ front-page.php
@@ -5,53 +5,53 @@
 
 	<!-- Banner Carousel Section -->
 	<?php 
-		// include get_theme_file_path('/inc/inc.banner_carousel.php'); 
+		include get_theme_file_path('/inc/inc.banner_carousel.php'); 
 	?>
 
 	<!-- Header Second Section -->
 	<?php 
-		// include get_theme_file_path( '/inc/inc.header_second.php' ); 
+		include get_theme_file_path( '/inc/inc.header_second.php' ); 
 	?>
 
 	
 	<!-- Know The Difference Section -->
 	<?php 
-		// include get_theme_file_path( '/inc/inc.know_the_difference.php' ); 
+		include get_theme_file_path( '/inc/inc.know_the_difference.php' ); 
 	?>
 
 	<!-- Info Counter Section -->
 	<?php 
-		// include get_theme_file_path('/inc/inc.info-counter.php'); 
+		include get_theme_file_path('/inc/inc.info-counter.php'); 
 	?>
 	
 	<!-- Best Sellers Section -->
 	<?php 
-		// include get_theme_file_path( '/inc/inc.best_sellers.php' );
+		include get_theme_file_path( '/inc/inc.best_sellers.php' );
 	?>
 
 	<!-- Why Choose Section -->
 	<?php 
-		// include get_theme_file_path( '/inc/inc.why_choose.php' );
+		include get_theme_file_path( '/inc/inc.why_choose.php' );
 	?>
 
 	<!-- Get Help Contact Section -->
 	<?php 
-		// include get_theme_file_path( '/inc/inc.get_help.php' ); 
+		include get_theme_file_path( '/inc/inc.get_help.php' ); 
 	?>
 
 	<!-- Testimonials Section -->
 	<?php 
-		// include get_theme_file_path( '/inc/inc.cjr_testimonials.php' ); 
+		include get_theme_file_path( '/inc/inc.cjr_testimonials.php' ); 
 	?>
 	
 	<!-- Client Carousel Section -->
 	<?php 
-		// include get_theme_file_path( '/inc/inc.client_carousel.php' ); 
+		include get_theme_file_path( '/inc/inc.client_carousel.php' ); 
 	?>
 	
 	<!-- Take Advantage Section -->
 	<?php 
-		// include get_theme_file_path( '/inc/inc.take_advantage.php' );
+		include get_theme_file_path( '/inc/inc.take_advantage.php' );
 	?>
 
 </div> <!-- cntr-top-content -->
