01b8aae57fea640ca1a3616385b3d063a8d43e02
Leo <leocsdev@gmail.com>
2020-11-16 10:40:31 +0800

    Updated layout for get help section.

--- front-page.php
+++ front-page.php
@@ -36,7 +36,7 @@
 
 	<!-- Get Help Contact Section -->
 	<?php 
-		// include get_theme_file_path( '/inc/inc.get_help.php' ); 
+		include get_theme_file_path( '/inc/inc.get_help.php' ); 
 	?>
 
 	<!-- Testimonials Section -->
--- inc/inc.get_help.php
+++ inc/inc.get_help.php
@@ -1,41 +1,59 @@
-<div class="container" style="color: black; margin-top: 50px">
+<section id="get-help">
+	<div class="container">
+
+		<h3 class="text-center">
+			Get help with choosing the right machine that <br>
+			will satisfy your business needs
+		</h3>
+		<br>
+		<p class="text-center">
+			If you are not sure about which of our cutting machines fits your requirements best, you can always reach us via phone, chat, or email.
+			From purchasing to setting up your press to diagnosing issues, CJR Support Team has you covered.
+		</p>
+
+		<!-- <div class="row" style="padding: 50px 50px 50px 50px"> -->
+		<div class="row my-3">
+
+			<!-- <div class="col-md-4" align="center"> -->
+			<div class="col-sm-4 text-center">
+				<div class="get-help-icons">
+					<i class="fa fa-comments-o" aria-hidden="true"></i>
+				</div>
+				<p>Get real-time assistance from a CJR expert right on your computer or mobile device.</p>
+			</div>
 
 
-	<div class="row" style="padding: 50px 50px 50px 50px">
+			<!-- <div class="col-md-4" align="center"> -->
+			<div class="col-sm-4 text-center">
+				<div class="get-help-icons">
+					<i class="fa fa-phone" aria-hidden="true"></i>
+				</div>
+				<p>You can call us anytime or schedule a time
+				for us to call you.</p>
+			</div>
 
-		<center>
-		<h3>Get help with choosing the right machine that <br>
-			will satisfy your business needs.</h3><br>
-		<p>If you are not sure about which of our cutting machines fits your requirements best, you can always reach us via phone, chat, or email.
-			From purchasing to setting up your press to diagnosing issues, CJR Support Team has you covered.</p>
-		</center>
 
+			<!-- <div class="col-md-4" align="center"> -->
+			<div class="col-sm-4 text-center">
+				<div class="get-help-icons">
+					<i class="fa fa-envelope-o" aria-hidden="true"></i>
+				</div>
+				<p>For questions about any of our products, send us an email and we’ll answer you as soon as possible.</p>
+			</div>
 
-		<div class="col-md-4" align="center">
-			<h1><i class="fa fa-comments-o" aria-hidden="true"></i></h1>
-			<p>Get real-time assistance from a CJR expert right on your computer or mobile device.</p>
 		</div>
 
+		<!-- <center>
+		<a href="<?php // site_url() ?>/contact" class="btn btn-success" style="align-content: center !important">Contact us now!</a>
+		</center> -->
 
-		<div class="col-md-4" align="center">
-			<h1><i class="fa fa-phone" aria-hidden="true"></i></h1>
-			<p>You can call us anytime or schedule a time
-			for us to call you.</p>
+		<div class="text-center">
+			<a 
+				href="<?php site_url() ?>/contact" 
+				class="btn btn-cjr"
+			>
+				Contact Us Now
+			</a>
 		</div>
-
-
-		<div class="col-md-4" align="center">
-		<h1><i class="fa fa-envelope-o" aria-hidden="true"></i></h1>
-			<p>For questions about any of our products, send us an email and we’ll answer you as soon as possible.</p>
-		</div>
-
 	</div>
-
-	<center>
-	<a href="<?php site_url() ?>/contact" class="btn btn-success" style="align-content: center !important">Contact us now!</a>
-	</center>
-
-</div>
-
-
-
+</section>
