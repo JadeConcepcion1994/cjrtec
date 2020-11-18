83f92b42c73dfc49b63c067b1c976170ac90f679
Leo <leocsdev@gmail.com>
2020-11-18 09:16:50 +0800

    Created a copy of footer for backup.

---
+++ footer copy.php
@@ -0,0 +1,409 @@
+<footer>
+	<section id="footer-cutting-press">
+		<div class="container">
+			<h2 class="text-center">
+				High-Quality Cutting Presses for Different Industries
+			</h2>
+		</div>
+
+		<div class="press-list">
+			<div class="container">
+				<div class="row">
+					<div class="col-6 col-md-3 py-3 d-sm-flex justify-content-center">
+						<ul>
+							<li><a href="beam-press.php">Beam Press</a></li>
+							<li><a href="traveling-head-press.php">Traveling Head Press</a></li>
+							<li><a href="receding-beam-press.php">Receding Beam Press</a></li>
+							<li><a href="auto-table-beam-press.php">Dual Feed Press</a></li>
+							<li><a href="wide-open.php">Plane Press</a></li>
+							<li><a href="traveling-head-press.php">CNC Traveling Head Press</a></li>
+							<li><a href="beam-press.php">Hydraulic Beam Press</a></li>
+							<li><a href="clicker-press.php">Automatic Cutting Press</a></li>
+							<li><a href="swing-arm-press.php">Shoe Press</a></li>
+							<li><a href="conveyor-press.php">Gasket Press</a></li>
+							<li><a href="beam-press.php">Plastics Press</a></li>
+							<li><a href="cutting-machines.php#convoluter">Foam Press</a></li>
+							<li><a href="conveyor-press.php">PET Press</a></li>
+							<li><a href="wide-open.php">4 Post Clicker Press</a></li>
+						</ul>
+					</div>
+
+					<div class="col-6 col-md-3 py-3 d-sm-flex justify-content-center">
+						<ul>
+							<li><a href="beam-press.php">Beam Cutting Press</a></li>
+							<li><a href="clicker-press.php">Auto Cutting Press</a></li>
+							<li><a href="swing-arm-press.php">Shoe Cutting Press</a></li>
+							<li><a href="conveyor-press.php">Gasket Cutting Press</a></li>
+							<li><a href="beam-press.php">Plastic Cutting Press</a></li>
+							<li><a href="videos.php">Die Cutting Press</a></li>
+							<li><a href="traveling-head-press.php">Paper Cutting Press</a></li>
+							<li><a href="cutting-machines.php#convoluter">Foam Cutting Press</a></li>
+							<li><a href="cutting-machines.php">Mold Forming Press</a></li>
+							<li><a href="wide-open.php">Impression Press</a></li>
+							<li><a href="conveyor-press.php">Plastic Welding Press</a></li>
+							<li><a href="cutting-machines.php">Heat Sealing Press</a></li>
+							<li><a href="wide-open.php">Plane Cutting Press</a></li>
+							<li><a href="clicker-press.php">Custom Clicker Press</a></li>
+						</ul>
+					</div>
+
+					<div class="col-6 col-md-3 py-3 d-sm-flex justify-content-center">
+						<ul>
+							<li><a href="swing-arm-press.php">Leather Clicker Press</a></li>
+							<li><a href="videos.php">Leather Die Cutting</a></li>
+							<li><a href="auto-table-beam-press.php">Auto Feed Cutting Press</a></li>
+							<li><a href="beam-press.php">4 Post Cutting Press</a></li>
+							<li><a href="wide-open.php">2 Post Cutting Press</a></li>
+							<li><a href="cutting-machines.php#waterjet">Flooring Cutting Press</a></li>
+							<li><a href="conveyor-press.php">200 Ton Press</a></li>
+							<li><a href="receding-beam-press.php">100 Ton Press</a></li>
+							<li><a href="traveling-head-press.php">80 Ton Press</a></li>
+							<li><a href="beam-press.php">50 Ton Press</a></li>
+							<li><a href="swing-arm-press.php">25 Ton Press</a></li>
+							<li><a href="swing-arm-press.php">27 Ton Swing Arm Press</a></li>
+							<li><a href="clicker-press.php">20 Ton Press</a></li>
+							<li><a href="swing-arm-press.php">10 Ton Press</a></li>
+						</ul>
+					</div>
+
+					<div class="col-6 col-md-3 py-3 d-sm-flex justify-content-center">
+						<ul>
+							<li><a href="clicker-press.php">Flooring Clicker Press</a></li>
+							<li><a href="cutting-boards.php">Die Press</a></li>
+							<li><a href="beam-press.php">Cork Cutting Press</a></li>
+							<li><a href="wide-open.php">Wood Cutting Press</a></li>
+							<li><a href="traveling-head-press.php">Bonding Press</a></li>
+							<li><a href="videos.php">Lamination Press</a></li>
+							<li><a href="swing-arm-press.php">10 Ton Clicker Press</a></li>
+							<li><a href="clicker-press.php">27 Ton Clicker Press</a></li>
+							<li><a href="traveling-head-press.php">40 Ton CLicker Press</a></li>
+							<li><a href="traveling-head-press.php">50 Ton Clicker PRess</a></li>
+							<li><a href="beam-press.php">80 Ton Clicker Press</a></li>
+							<li><a href="receding-beam-press.php">100 Ton Clicker Press</a></li>
+							<li><a href="conveyor-press.php">200 Ton Clicker Press</a></li>
+							<li><a href="swing-arm-press.php">25 Ton Swing Arm Press</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<section id="footer-banner">		
+		<div class="container">
+			<div 
+				class="banner"
+				style="background: url('<?php bloginfo('template_directory'); ?>/assets/img/cnc-press-cutting.jpg'); 
+				background-repeat: no-repeat;
+				background-size: cover;
+				background-position: center;"
+			>
+				<div class="banner-content">
+					<h3 class="mb-1">Take full advantage of automation.</h3>
+					<p class="mb-4">
+						Get the most out of your manufacturing business through CJRTec
+						Automated Engineering.
+					</p>
+					<a href="#registration">
+						Register now and see for yourself
+						<i class="fa fa-angle-right"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="footer-list">
+		<div class="container">
+			<div class="row row-cjr-list">
+				<div class="col-6 col-sm-4 col-md-2 my-3">
+					<p class="ptitle">About CJRTEC</p>
+					<ul>
+						<li>
+							<a href="<?php echo site_url() ?>/about">
+								About us
+							</a>
+						</li>
+						<li>
+							<a href="#testimonials">
+								Testimonials
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url() ?>/distributors">
+								Distributors
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url() ?>/customers">
+								Customers
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url() ?>/contact">
+								Contact us
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="col-6 col-sm-4 col-md-2 my-3">
+					<p class="ptitle">Account</p>
+					<ul>
+						<li>
+							<a href="#login">
+								Login
+							</a>
+						</li>
+						<li>
+							<a href="#registration">
+								Registration
+							</a>
+						</li>
+						<li>
+							<a href="#forgot-password">
+								Forgot Password
+							</a>
+						</li>
+						<li>
+							<a href="#unsubscribe">
+								Unsubscribe
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="col-6 col-sm-4 col-md-2 my-3">
+					<p class="ptitle">Products</p>
+					<ul>
+						<li>
+							<a href="clicker-press.php">
+								Clicker Press
+							</a>
+						</li>
+						<li>
+							<a href="http://www.digitalcuttingsystems.com/" target="_blank">
+								Waterjet Cutting
+							</a>
+						</li>
+						<li>
+							<a href="http://www.convolutermachine.com/" target="_blank">
+								Foam Cutting
+							</a>
+						</li>
+						<li>
+							<a href="http://www.itbotics.com/" target="_blank">
+								Industrial Robots
+							</a>
+						</li>
+						<li>
+							<a href="http://www.hotmeltcoatingmachines.com/">
+								Hotmelt Laminator
+							</a>
+						</li>
+						<li>
+							<a href="http://www.industrialknive.com/" target="_blank">
+								Industrial Knives
+							</a>
+						</li>
+						<li>
+							<a href="clicker-press-parts.php">
+								Press Parts
+							</a>
+						</li>
+						<li>
+							<a href="accessories.php">
+								Accessories
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="col-6 col-sm-4 col-md-2 my-3">
+					<p class="ptitle">Useful Links</p>
+					<ul>
+						<li>
+							<a href="<?php echo site_url() ?>/training">
+								Trainings
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url() ?>/referrals">
+								Referrals
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url() ?>/clicker-press-shipping">
+								Shipping
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url() ?>/franchise">
+								Franchise
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url() ?>/finance">
+								Financing
+							</a>
+						</li>
+						<li>
+							<a href="http://cjrterms.com/" target="_blank">
+								Purchase Terms
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url() ?>/warranty">
+								Warranty
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url() ?>/tonnage-formula">
+								Tonnage Formula
+							</a>
+						</li>
+					</ul>
+				</div>
+				
+				<div class="col-6 col-sm-4 col-md-2 my-3">
+					<p class="ptitle">Learn More</p>
+					<ul>
+						<li>
+							<a href="<?php echo site_url() ?>/types-of-dies">
+								Types of Dies
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url() ?>/used-clicker-press">
+								Used Presses
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url() ?>/die-makers">
+								Die makers
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url() ?>/videos">
+								Videos
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url() ?>/china-steel">
+								China Steel
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url() ?>/buyback">
+								Buy Back Protection
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url() ?>/commercial-die-cutting">
+								Commercial Die Cutting
+							</a>
+						</li>
+						<li>
+							<a href="<?php echo site_url(); ?>/swing-arm-press-testing">
+								Swing Arm Press Testing
+							</a>
+						</li>
+					</ul>
+				</div>	
+				
+				<div class="col-6 col-sm-4 col-md-2 my-3">
+					<p class="ptitle">Find us here</p>
+
+					<ul class="social">
+						<li>
+							<a href="https://www.facebook.com/cjrtec/" target="_blank">
+								<i class="fa fa-facebook-official fa-lg"></i> 
+								<div><span>Connect to us</span></div>
+							</a>
+						</li>
+						<li>
+							<a href="https://www.instagram.com/cjrtec" target="_blank">
+								<i class="fa fa-instagram fa-lg"></i> 
+								<div><span>View our feed</span></div>
+							</a>
+						</li>
+						<li>
+							<a href="https://www.youtube.com/channel/UCNph4b5fxTNKhQnycRp4oyw" target="_blank">
+								<i class="fa fa-youtube fa-lg"></i> 
+								<div><span>Watch our Videos</span></div>
+							</a>
+						</li>
+					</ul>
+				</div>
+
+			</div> <!-- row row-cjr-list -->
+
+			<div class="row row-footer-content">
+				<div class="col-12">
+					<p class="text-center">
+						Call 
+						<a href="tel:+18007332302">
+							<strong>(800)-733-2302</strong>
+						</a> 
+						or
+						<a href="mailto:support@cjrtec.com">
+							<strong>email support@cjrtec.com</strong> 
+						</a>
+						<br> 
+						for more info.
+					</p>
+					<hr style="background-color: white">
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-4">
+					<ul id="ul_li_inline">
+						<li>
+							<a href="http://cjrterms.com/" target="_blank">
+								Terms
+							</a>
+							&nbsp; | &nbsp;
+						</li>
+						<li>
+							<a href="<?php echo site_url(); ?>/warranty">
+								Warranty 
+							</a>
+							&nbsp; | &nbsp;
+						</li>
+						<li>
+							<a href="<?php echo site_url(); ?>/buyback">
+								Buy Back Protection
+							</a>
+						</li>
+						<!-- <li>
+							<a href="links.php">Links</a>
+						</li> -->
+					</ul>
+				</div>
+
+				<div class="col-md-4 col-copyright-footer">
+					<p class="text-center">
+						Copyright © <?php echo date('Y'); ?> 		
+						<a href="<?php echo site_url(); ?>">
+							<span style=" color: rgb(255, 88, 41)">
+								CJRTec
+							</span>
+						</a>
+						. All rights reserved.
+					</p>
+				</div>
+
+				<!-- <div class="col-md-4">
+					<button onclick="topFunction()"  id="myBtn" title="Go to top" ><i class="fa fa-chevron-up" aria-hidden="true"></i></button>
+				</div> -->
+			</div>
+		</div> <!-- container -->
+	</section>
+
+</footer>
+
+<?php wp_footer(); ?>	
+
+</body>
+</html>
